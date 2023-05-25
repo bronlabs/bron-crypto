bd0490e3b6bf7819070081c4d9370bb29d232faf
Alireza Rafiei <alireza.rafiei@copper.co>
2023-05-25 15:49:22 +0100

    comment

--- pkg/signatures/schnorr/schnorr.go
+++ pkg/signatures/schnorr/schnorr.go
@@ -116,8 +116,6 @@ func Verify(cipherSuite *integration.CipherSuite, publicKey *PublicKey, message 
 		if !ok {
 			return errors.New("curve is ed25519 but the public key could not be type casted to the correct point struct")
 		}
-		// this check is not part of the ed25519 standard yet if the public key is of small order then the signature will be susceptibe
-		// to a key substitution attack (specifically, it won't be binded to a public key (SBS) and a signature cannot be binded to a unique message in presence of malicious keys (MBS)). Refer to section 5.4 of https://eprint.iacr.org/2020/823.pdf and https://eprint.iacr.org/2020/1244.pdf
 		if edwardsPoint.IsSmallOrder() {
 			return errors.New("public key is small order")
 		}
