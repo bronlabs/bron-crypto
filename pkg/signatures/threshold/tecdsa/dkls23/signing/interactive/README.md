867c38d501b4e9d63b19019ef54ac6812686becc
Alireza Rafiei <alireza.rafiei@copper.co>
2023-08-07 19:14:58 +0100

    dkls23 signing

---
+++ pkg/signatures/threshold/tecdsa/dkls23/signing/interactive/README.md
@@ -0,0 +1,97 @@
+# DKLs23 Interactive Signing
+
+This package implements the 3-round, (t, n) signing protocol of [DKLs23](https://eprint.iacr.org/2023/765.pdf), realizing the standard ECDSA functionality defined as Functionality 3.1 in the paper.
+
+The details of the protocol are sketched in Protocol 3.6 of the main paper. We are largely faithful to the original paper except:
+1. We have confirmation from the DKLs team that the 2nd consistency check of the paper has a typo, which we fix.
+2. We'll compute the recovery id and extend the ECDSA signature with it.
+3. $R_i$ is included in the partial signature, for the aggregator to compute the sum itself, as well as be able to compute the recovery id independently.
+
+
+## Configuration
+
+**Players**:
+- `n` players where at least `t` of them are present during the signing session.
+- At least one signature aggregator, who may or may not be in the cohort.
+
+**Parameters**:
+- Follows whatever is defined in the subprotocols used.
+
+**Functionalities**:
+- `RVOLE` The two party multiplication protocol.
+- `Zero` The zero share sampling protocol.
+- `Commit` Commitment functionality which can commit and open
+- `H(x,L)` Hash function, input x of variable size, output of size $\mathbb{Z}_q^L$. For ECDSA, this is typically SHA2.
+- `Send(x)=> P` Send message x to party P.
+- `Broadcast(x)` Echo-broadcasts x to all parties.
+
+**Input**:
+- UniqueSessionId
+- Message $m$ (given in the 3rd round)
+
+**Output**:
+- Partial Signature (end of round 3)
+- Signature (end of aggregation - which may happen immediately after round 3)
+
+## Protocol:
+
+This protocol is symmetric: In every round, all parties do the same thing.
+
+0. Init:
+    1. DKG
+    2. Zero sahre sampling seed.
+    3. Compute COTe.Setup (init S&R in SoftspokenOT with [ $\kappa \times$ BaseOT] seeds) and construct pairwise multiplication instances.
+
+1. Round 1:
+    1. Compute inversion mask $phi_i \leftarrow \mathbb{Z}_q$
+    2. Compute instance key $r_i \leftarrow \mathbb{Z}_q$
+    3. Compute commitment to instance key: $R_i = r_i \cdot G$
+    3. For all present parties p_j:
+        1. $c, w = Commit(p_i, p_j, sid, R_i)$ where `c` is the commitment and `w` the witness. Store `w`.
+        2. Run first round of `RVOLE` as bob and record the output as `multiplicationOutput`.
+        3. Record Bob's input (with forced-resuse it'll be $\tilde{b}$) as $\chi_{ij}$
+        4. `Send(c, multiplicationOutput) => P_j`
+    4. `Broadcast(R_i)`
+
+2. Round 2:
+    1. Receive $\zeta_i$ from the zero share sampling subprotocol.
+    2. Convert shamir share of the private key to additive to get `a`.
+    3. Compute $sk_i=a+\zeta_i$
+    4. Compute $pk_i=sk_i \cdot G$
+    5. Set multiplication input $a = \{r_i, sk_i\}
+    6. For all present parties p_j:
+        1. Receive broadcasted `R_ij`
+        2. Receive unicasted `commitment_j` and `multiplicationOutput_j`
+        3. Run 2nd round of `RVOLE` as Alice and record the output as $c^u_{ij}$ and $c^v_{ij}$ as well as `multiplicationOutput`.
+        4. Set $\Gamma^u_{ij} = c^u_{ij} \cdot G$
+        5. Set $\Gamma^v_{ij} = c^v_{ij} \cdot G$
+        6. Set $\psi_{ij} = \phi_i - \chi_{ij}$
+        7. Send the `multiplicationOutput`, $\Gamma^u_{ij}$, $\gamma^v_{ij}$, $\psi_{ij}$ and opening of the commitment `c` corresponding to p_j.
+    7. Broadcast $pk_i$.
+
+3. Round 3 (ABORTs here, like all our protocols, are global and should affect all running sessions):
+    1. For all present parties p_j:
+        1. Receive $pk_j$.
+        2. Receive $\Gamma^u_{ji}$ and $\gamma^v_{ji}$, `multiplicationOutput` and the witness.
+        3. **ABORT** if $(p_j, p_i, sid, R_j)$ can't be opened.
+        4. Run the 3rd round of `RVOLE` as bob with the received `multiplicationOutput` to get $d^u_{ij}$ and $d^v_{ij}$.
+        5. **ABORT** if $\chi_{ij} \cdot R_j - \Gamma^u_{ji} \neq d^u_{ij} \cdot G$
+        6. **ABORT** if $\chi_{ij} \cdot pk_i - \Gamma^v_{ji} \neq d^v_{ij} \cdot G$
+    2. **ABORT** if sum of all $pk_j$ as well as $pk_i$ is not equal to the public key.
+    3. Compute $R = \sum R_j$
+    4. Compute $u_j = r_i \cdot (\phi_i + \sum psi_{ji}) + \sum (c^u_{ij} + d^u_{ij})$
+    5. Compute $v_j = sk_i \cdot (\phi_i + \sum psi_{ji}) + \sum (c^v_{ij} + d^v_{ij})$
+    6. Compute $w_i = SHA2(m) \cdot \phi_i + (R.x) \cdot v_i$ where $(R.x)$ is the x-coordinate of $.
+    7. Broadcasts $u_i$ and $w_i$.
+
+4. Aggregation
+   1. Receive $u_i$, $w_i$ and $R_i$.
+   2. Set $r$ to be x coodinate of $\sum R_i$
+   3. Set $s = \cfrac{\sum w_i}{\sum u_i}$
+   4. Comput recovery id `v`
+   5. Set $\sigma=(v, r, s)$.
+   6. Normalize the signature (convert to low-s form and adjust `v`)
+   7. **ABORT** if $\sigma$ can't be verified.
+   8. Output $\sigma$
+
+
---
+++ pkg/signatures/threshold/tecdsa/dkls23/signing/interactive/rounds.go
@@ -0,0 +1,307 @@
+package interactive
+
+import (
+	"bytes"
+	"math/big"
+
+	"github.com/copperexchange/knox-primitives/pkg/commitments"
+	"github.com/copperexchange/knox-primitives/pkg/core/curves"
+	"github.com/copperexchange/knox-primitives/pkg/core/errs"
+	"github.com/copperexchange/knox-primitives/pkg/core/hashing"
+	"github.com/copperexchange/knox-primitives/pkg/core/integration"
+	"github.com/copperexchange/knox-primitives/pkg/sharing/shamir"
+	"github.com/copperexchange/knox-primitives/pkg/signatures/ecdsa"
+	"github.com/copperexchange/knox-primitives/pkg/signatures/threshold/tecdsa/dkls23"
+	"github.com/copperexchange/knox-primitives/pkg/signatures/threshold/tecdsa/dkls23/mult"
+	"golang.org/x/crypto/sha3"
+)
+
+var h = sha3.New256
+
+type Round1Broadcast struct {
+	R_i curves.Point
+}
+
+type Round1P2P struct {
+	CommitmentToInstanceKey commitments.Commitment
+	MultiplicationOutput    *mult.Round1Output
+}
+
+type Round2P2P struct {
+	Multiplication                      *mult.Round2Output
+	GammaU_ij                           curves.Point
+	GammaV_ij                           curves.Point
+	Psi_ij                              curves.Scalar
+	WitnessOfTheCommitmentToInstanceKey commitments.Witness
+}
+
+type Round2Broadcast struct {
+	PK_i curves.Point
+}
+
+func (ic *Cosigner) Round1() (*Round1Broadcast, map[integration.IdentityKey]*Round1P2P, error) {
+	// step 1.1
+	ic.state.phi_i = ic.CohortConfig.CipherSuite.Curve.Scalar.Random(ic.prng)
+	// step 1.2
+	ic.state.r_i = ic.CohortConfig.CipherSuite.Curve.Scalar.Random(ic.prng)
+	// step 1.3
+	ic.state.R_i = ic.CohortConfig.CipherSuite.Curve.ScalarBaseMult(ic.state.r_i)
+
+	outputP2P := make(map[integration.IdentityKey]*Round1P2P, len(ic.SessionParticipants))
+	for _, participant := range ic.SessionParticipants {
+		if participant.PublicKey().Equal(ic.MyIdentityKey.PublicKey()) {
+			continue
+		}
+
+		// step 1.3.1
+		toBeCommitted := bytes.Join([][]byte{
+			{
+				byte(ic.MyShamirId),
+				byte(ic.IdentityKeyToShamirId[participant]),
+			}, ic.UniqueSessionId,
+			ic.state.R_i.ToAffineCompressed(),
+		}, []byte(""))
+		commitmentToInstanceKey, witness, err := commitments.Commit(h, toBeCommitted)
+		ic.state.witnessesOfCommitmentToInstanceKey[participant] = witness
+
+		// step 1.3.2
+		multiplicationOutput, err := ic.subprotocols.multiplication[participant].Bob.Round1()
+		if err != nil {
+			return nil, nil, errs.WrapFailed(err, "multiplication round 1")
+		}
+
+		// step 1.3.3
+		ic.state.Chi_i[participant] = ic.subprotocols.multiplication[participant].Bob.BTilde[0]
+		if ic.subprotocols.multiplication[participant].Bob.BTilde[0].Cmp(ic.subprotocols.multiplication[participant].Bob.BTilde[1]) != 0 {
+			return nil, nil, errs.WrapFailed(err, "bob's input is not compatible with forced reuse")
+		}
+
+		// step 1.3.4
+		outputP2P[participant] = &Round1P2P{
+			CommitmentToInstanceKey: commitmentToInstanceKey,
+			MultiplicationOutput:    multiplicationOutput,
+		}
+	}
+	// step 1.4
+	return &Round1Broadcast{
+		R_i: ic.state.R_i,
+	}, outputP2P, nil
+}
+
+func (ic *Cosigner) Round2(round1outputBroadcast map[integration.IdentityKey]*Round1Broadcast, round1outputP2P map[integration.IdentityKey]*Round1P2P) (*Round2Broadcast, map[integration.IdentityKey]*Round2P2P, error) {
+	// step 2.1
+	zeta_i, err := ic.subprotocols.zeroShareSampling.Sample()
+	if err != nil {
+		return nil, nil, errs.WrapFailed(err, "mask F_Zero")
+	}
+	// step 2.2
+	myShamirShare := &shamir.Share{
+		Id:    ic.MyShamirId,
+		Value: ic.Shard.SigningKeyShare.Share,
+	}
+	myAdditiveShare, err := myShamirShare.ToAdditive(ic.sessionShamirIDs)
+	if err != nil {
+		return nil, nil, errs.WrapFailed(err, "could not convert my shamir share to additive share")
+	}
+	// step 2.3
+	ic.state.sk_i = myAdditiveShare.Add(zeta_i)
+	// step 2.4
+	ic.state.pk_i = ic.CohortConfig.CipherSuite.Curve.ScalarBaseMult(ic.state.sk_i)
+	// step 2.5
+	a := [mult.L]curves.Scalar{ic.state.r_i, ic.state.sk_i}
+
+	outputP2P := make(map[integration.IdentityKey]*Round2P2P)
+	for _, participant := range ic.SessionParticipants {
+		if participant.PublicKey().Equal(ic.MyIdentityKey.PublicKey()) {
+			continue
+		}
+		// step 2.6.1
+		receivedBroadcastMessage := round1outputBroadcast[participant]
+		ic.state.receivedR_i[participant] = receivedBroadcastMessage.R_i
+
+		// step 2.6.2
+		receivedP2PMessage := round1outputP2P[participant]
+		ic.state.receivedCommitmentsToInstanceKey[participant] = receivedP2PMessage.CommitmentToInstanceKey
+
+		// step 2.6.3
+		c_ij, multiplicationOutput, err := ic.subprotocols.multiplication[participant].Alice.Round2(receivedP2PMessage.MultiplicationOutput, a)
+		if err != nil {
+			return nil, nil, errs.WrapFailed(err, "F_RVOLE round 2 sample")
+		}
+		ic.state.cU_i[participant] = c_ij[0]
+		ic.state.cV_i[participant] = c_ij[1]
+
+		// step 2.6.4
+		gammaU_ij := ic.CohortConfig.CipherSuite.Curve.ScalarBaseMult(ic.state.cU_i[participant])
+		// step 2.6.5
+		gammaV_ij := ic.CohortConfig.CipherSuite.Curve.ScalarBaseMult(ic.state.cV_i[participant])
+		// step 2.6.6
+		psi_ij := ic.state.phi_i.Sub(ic.state.Chi_i[participant])
+
+		// step 2.6.7
+		outputP2P[participant] = &Round2P2P{
+			Multiplication:                      multiplicationOutput,
+			GammaU_ij:                           gammaU_ij,
+			GammaV_ij:                           gammaV_ij,
+			Psi_ij:                              psi_ij,
+			WitnessOfTheCommitmentToInstanceKey: ic.state.witnessesOfCommitmentToInstanceKey[participant]}
+	}
+	// step 2.7
+	return &Round2Broadcast{
+		PK_i: ic.state.pk_i,
+	}, outputP2P, nil
+}
+
+func (ic *Cosigner) Round3(round2outputBroadcast map[integration.IdentityKey]*Round2Broadcast, round2outputP2P map[integration.IdentityKey]*Round2P2P, message []byte) (*dkls23.PartialSignature, error) {
+	recomputedPublicKey := ic.state.pk_i // Zeta should cancel out
+	R := ic.state.R_i
+	phiPsi := ic.state.phi_i
+	cUdU := ic.CohortConfig.CipherSuite.Curve.Scalar.Zero()
+	cVdV := ic.CohortConfig.CipherSuite.Curve.Scalar.Zero()
+
+	for _, participant := range ic.SessionParticipants {
+		if participant.PublicKey().Equal(ic.MyIdentityKey.PublicKey()) {
+			continue
+		}
+		// step 3.1.1
+		receivedBroadcastedMessage, exists := round2outputBroadcast[participant]
+		if !exists {
+			return nil, errs.NewMissing("don't have broadcast message")
+		}
+		pk_j := receivedBroadcastedMessage.PK_i
+
+		// step 3.1.2
+		receivedP2PMessage, exists := round2outputP2P[participant]
+		if !exists {
+			return nil, errs.NewMissing("don't have p2p message")
+		}
+		GammaU_ji := receivedP2PMessage.GammaU_ij
+		GammaV_ji := receivedP2PMessage.GammaV_ij
+
+		// step 3.1.3
+		supposedlyCommittedMessage := bytes.Join([][]byte{
+			{
+				byte(ic.IdentityKeyToShamirId[participant]),
+				byte(ic.MyShamirId),
+			}, ic.UniqueSessionId,
+			ic.state.receivedR_i[participant].ToAffineCompressed(),
+		}, []byte(""))
+
+		if err := commitments.Open(
+			h,
+			supposedlyCommittedMessage,
+			ic.state.receivedCommitmentsToInstanceKey[participant],
+			receivedP2PMessage.WitnessOfTheCommitmentToInstanceKey,
+		); err != nil {
+			return nil, errs.WrapIdentifiableAbort(err, "message could not be openned")
+		}
+
+		// step 3.1.4
+		d_ij, err := ic.subprotocols.multiplication[participant].Bob.Round3(receivedP2PMessage.Multiplication)
+		if err != nil {
+			return nil, errs.WrapFailed(err, "bob round 3")
+		}
+		du_ij := d_ij[0]
+		dv_ij := d_ij[1]
+
+		Chi_ij := ic.state.Chi_i[participant]
+		// step 3.1.5
+		R_j := ic.state.receivedR_i[participant]
+		lhs1 := R_j.Mul(Chi_ij).Sub(GammaU_ji)
+		rhs1 := ic.CohortConfig.CipherSuite.Curve.ScalarBaseMult(du_ij)
+		if !lhs1.Equal(rhs1) {
+			return nil, errs.NewIdentifiableAbort("failed first check")
+		}
+
+		// step 3.1.6
+		P_j := pk_j
+		llhs := P_j.Mul(Chi_ij)
+		lhs2 := llhs.Sub(GammaV_ji)
+		rhs := ic.CohortConfig.CipherSuite.Curve.ScalarBaseMult(dv_ij)
+		if !lhs2.Equal(rhs) {
+			return nil, errs.NewIdentifiableAbort("failed second check")
+		}
+
+		recomputedPublicKey = recomputedPublicKey.Add(pk_j)
+		// We're partially evaluating what we need for future steps inside of this loop
+		cu_ij := ic.state.cU_i[participant]
+		cv_ij := ic.state.cV_i[participant]
+		phiPsi = phiPsi.Add(round2outputP2P[participant].Psi_ij)
+		cUdU = cUdU.Add(cu_ij).Add(du_ij)
+		cVdV = cVdV.Add(cv_ij).Add(dv_ij)
+		R = R.Add(R_j) // step 3.3
+	}
+
+	// step 3.2
+	if !recomputedPublicKey.Equal(ic.Shard.SigningKeyShare.PublicKey) {
+		return nil, errs.NewFailed("recomputed public key is wrong")
+	}
+
+	// step 3.4
+	u_i := ic.state.r_i.Mul(phiPsi).Add(cUdU)
+	// step 3.5
+	v_i := ic.state.sk_i.Mul(phiPsi).Add(cVdV)
+
+	// step 3.6
+	xBigInt, _ := getPointCoordinates(R)
+	rx, err := ic.CohortConfig.CipherSuite.Curve.Scalar.SetBigInt(xBigInt)
+	if err != nil {
+		return nil, errs.WrapFailed(err, "rx")
+	}
+	// TODO: FiatShamir is not the right name. Clean up hash to curve scalar stuff. Alternative is regular hash then
+	// setbytefunc
+	digest, err := hashing.FiatShamir(ic.CohortConfig.CipherSuite, message)
+	if err != nil {
+		return nil, errs.WrapFailed(err, "could not produce digest")
+	}
+	w_i := digest.Mul(ic.state.phi_i).Add(rx.Mul(v_i))
+
+	// step 3.7
+	return &dkls23.PartialSignature{
+		Ui: u_i,
+		Wi: w_i,
+		Ri: ic.state.R_i,
+	}, nil
+}
+
+func Aggregate(cipherSuite *integration.CipherSuite, publicKey curves.Point, partialSignatures map[integration.IdentityKey]*dkls23.PartialSignature, message []byte) (*ecdsa.Signature, error) {
+	curve := cipherSuite.Curve
+	w := curve.Scalar.Zero()
+	u := curve.Scalar.Zero()
+	R := curve.Point.Identity()
+	for _, partialSignature := range partialSignatures {
+		w = w.Add(partialSignature.Wi)
+		u = u.Add(partialSignature.Ui)
+		R = R.Add(partialSignature.Ri)
+	}
+	xBigInt, _ := getPointCoordinates(R)
+
+	// step 4.2
+	rx, err := curve.Scalar.SetBigInt(xBigInt)
+	if err != nil {
+		return nil, errs.WrapFailed(err, "rx")
+	}
+	// step 4.3
+	s := w.Div(u)
+	// step 4.4
+	v, err := ecdsa.CalculateRecoveryId(R)
+	if err != nil {
+		return nil, errs.WrapFailed(err, "could not compute recovery id")
+	}
+	// step 4.5
+	sigma := &ecdsa.Signature{V: &v, R: rx, S: s}
+	// step 4.6
+	sigma.Normalise()
+	// step 4.7
+	if err := ecdsa.Verify(sigma, cipherSuite.Hash, publicKey, message); err != nil {
+		return nil, errs.WrapVerificationFailed(err, "sigma is invalid")
+	}
+	// step 4.8
+	return sigma, nil
+}
+
+// TODO: remove when curve interface is extended.
+func getPointCoordinates(point curves.Point) (x *big.Int, y *big.Int) {
+	affine := point.ToAffineUncompressed()
+	return new(big.Int).SetBytes(affine[1:33]), new(big.Int).SetBytes(affine[33:65])
+}
