ea1a57cbbf201e6da8034d10d0d8d7d60bbd0ba5
Moe Sarraf <mohammad.sarraf@copper.co>
2024-04-19 06:33:10 -0400

    input location updated

--- pkg/base/bitstring/bitstring_test.go
+++ pkg/base/bitstring/bitstring_test.go
@@ -8,117 +8,182 @@ import (
 	"github.com/copperexchange/krypton-primitives/pkg/base/bitstring"
 )
 
-func TestReverseBytes(t *testing.T) {
-	
-	t.Run("test for non empty array", func(t *testing.T){
+func TestReverseBytes1(t *testing.T) {
+
+	inputMatrix := [][]byte{
+		{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
+		{},
+	}
+
+	t.Run("test for non empty array", func(t *testing.T) {
 		t.Parallel()
 
-		inputMatrix :=  []byte {0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC}
-
 		//reverse of revese should be the same os original
-		result := bitstring.ReverseBytes(inputMatrix)
+		result := bitstring.ReverseBytes(inputMatrix[0])
 		result = bitstring.ReverseBytes(result)
 
-		require.Equal(t, result, inputMatrix)
+		require.Equal(t, result, inputMatrix[0])
 	})
 
 	t.Run("test for empty array", func(t *testing.T) {
 		t.Parallel()
 
-		inputMatrix :=  []byte {}
-        result := bitstring.ReverseBytes(inputMatrix)
+		result := bitstring.ReverseBytes(inputMatrix[1])
 
-        require.Equal(t, result, inputMatrix)
+		require.Equal(t, result, inputMatrix[1])
 	})
-//super large array??
+
 }
 
-func TestPadToLeft(t *testing.T) {
+func TestReverseBytes2(t *testing.T) {
 
+	testCase := []struct {
+		name        string
+		inputMatrix []byte
+		// expected []byte
+	}{
+		{
+			"test for non empty array",
+			[]byte{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
+		},
+		{
+			"test for an empty array",
+			[]byte{},
+		},
+	}
+
+	for _, tc := range testCase {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			//reverse of revese should be the same os original
+			result := bitstring.ReverseBytes(tc.inputMatrix)
+			result = bitstring.ReverseBytes(result)
+
+			require.Equal(t, result, tc.inputMatrix)
+		})
+	}
+
+}
+
+func TestPadToLeft1(t *testing.T) {
+
+	inputMatrix := [][]byte{
+		{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
+		{},
+		{0x21},
+	}
 	t.Run("Test with positive padLen", func(t *testing.T) {
 		t.Parallel()
 
-		inputMatrix :=  [][]byte {
-			{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
-			{},
-			{0x21},
-		}
 		inputPadLengths := 2
-		expectedOutput := [][]byte {
+		expectedOutput := [][]byte{
 			{0x00, 0x00, 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
 			{0x00, 0x00},
 			{0x00, 0x00, 0x21},
 		}
 
-		for i:= 0; i < len(inputMatrix); i++ {
+		for i := 0; i < len(inputMatrix); i++ {
 			require.Equal(t, expectedOutput[i], bitstring.PadToLeft(inputMatrix[i], inputPadLengths))
 		}
 	})
 	t.Run("Test with negative padLen", func(t *testing.T) {
 		t.Parallel()
 
-		inputMatrix :=  [][]byte {
-			{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
-			{},
-			{0x21},
-		}
 		inputPadLengths := -2
 		excpectedOutput := inputMatrix
 
-		for i:= 0; i < len(inputMatrix); i++ {
+		for i := 0; i < len(inputMatrix); i++ {
 			require.Equal(t, excpectedOutput[i], bitstring.PadToLeft(inputMatrix[i], inputPadLengths))
 		}
 	})
 
 	t.Run("Test with zero padLen", func(t *testing.T) {
 		t.Parallel()
-
-		inputMatrix :=  [][]byte {
-			{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
-			{},
-			{0x21},
-		}
 		inputPadLengths := 0
 		excpectedOutput := inputMatrix
 
-		for i:= 0; i < len(inputMatrix); i++ {
+		for i := 0; i < len(inputMatrix); i++ {
 			require.Equal(t, excpectedOutput[i], bitstring.PadToLeft(inputMatrix[i], inputPadLengths))
 		}
 	})
 }
 
+func TestPadToLeft2(t *testing.T) {
+
+	testCases := []struct {
+		name            string
+		inputMatrix     [][]byte
+		inputPadLengths int
+		expectedOutput  [][]byte
+	}{
+		{
+			"Test with positive padLen",
+			[][]byte{
+				{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
+				{},
+				{0x21},
+			},
+			2,
+			[][]byte{
+				{0x00, 0x00, 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
+				{0x00, 0x00},
+				{0x00, 0x00, 0x21},
+			},
+		},
+		{
+			"Test with positive padLen",
+			[][]byte{
+				{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
+				{},
+				{0x21},
+			},
+			0,
+			[][]byte{
+				{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
+				{},
+				{0x21},
+			},
+		},
+	}
+	for _, tc := range testCases {
+		for i := range tc.inputMatrix {
+			t.Run(tc.name, func(t *testing.T) {
+				t.Parallel()
+
+				require.Equal(t, tc.expectedOutput[i], bitstring.PadToLeft(tc.inputMatrix[i], tc.inputPadLengths))
+			})
+		}
+	}
+}
+
 func TestPadToRight(t *testing.T) {
 
+	inputMatrix := [][]byte{
+		{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
+		{},
+		{0x21},
+	}
 	t.Run("Test with positive padLen", func(t *testing.T) {
 		t.Parallel()
 
-		inputMatrix :=  [][]byte {
-			{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
-			{},
-			{0x21},
-		}
 		inputPadLengths := 2
-		expectedOutput := [][]byte {
+		expectedOutput := [][]byte{
 			{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0x00, 0x00},
 			{0x00, 0x00},
 			{0x21, 0x00, 0x00},
 		}
-		for i:= 0; i < len(inputMatrix); i++ {
+		for i := 0; i < len(inputMatrix); i++ {
 			require.Equal(t, expectedOutput[i], bitstring.PadToRight(inputMatrix[i], inputPadLengths))
 		}
 	})
 	t.Run("Test with negative padLen", func(t *testing.T) {
 		t.Parallel()
 
-		inputMatrix :=  [][]byte {
-			{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
-			{},
-			{0x21},
-		}
 		inputPadLengths := -2
 		excpectedOutput := inputMatrix
 
-		for i:= 0; i < len(inputMatrix); i++ {
+		for i := 0; i < len(inputMatrix); i++ {
 			require.Equal(t, excpectedOutput[i], bitstring.PadToRight(inputMatrix[i], inputPadLengths))
 		}
 	})
@@ -126,14 +191,14 @@ func TestPadToRight(t *testing.T) {
 	t.Run("Test with zero padLen", func(t *testing.T) {
 		t.Parallel()
 
-		inputMatrix :=  [][]byte {
+		inputMatrix := [][]byte{
 			{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC},
 			{},
 			{0x21},
 		}
 		inputPadLengths := 0
 		excpectedOutput := inputMatrix
-		for i:= 0; i < len(inputMatrix); i++ {
+		for i := 0; i < len(inputMatrix); i++ {
 			require.Equal(t, excpectedOutput[i], bitstring.PadToRight(inputMatrix[i], inputPadLengths))
 		}
 	})
@@ -163,14 +228,14 @@ func TestTransposeBooleanMatrix(t *testing.T) {
 				// using bitstring.SelectBit (careful! it takes a byte array as input)
 				output1 := bitstring.PackedBits(inputMatrix[i]).Select(j)
 				output2 := bitstring.PackedBits(transposedMatrix[j]).Select(i)
-	
+
 				require.Equal(t,
 					output1,
 					output2)
 			}
-		}    
+		}
 	})
-	t.Run("Test for input not having rows%8==0", func(t *testing.T){
+	t.Run("Test for input not having rows%8==0", func(t *testing.T) {
 		t.Parallel()
 
 		inputMatrix := [][]byte{
@@ -185,7 +250,7 @@ func TestTransposeBooleanMatrix(t *testing.T) {
 		_, err := bitstring.TransposePackedBits(inputMatrix)
 		require.Error(t, err)
 	})
-	t.Run("Testing 1D matrix", func(t *testing.T){
+	t.Run("Testing 1D matrix", func(t *testing.T) {
 		t.Parallel()
 
 		inputMatrix := [][]byte{
@@ -198,107 +263,103 @@ func TestTransposeBooleanMatrix(t *testing.T) {
 
 func TestByteSubLE(t *testing.T) {
 
+	inputMatrix := [][]byte{
+		{0x05, 0x01, 0x00, 0x00},
+		{0x00, 0x00, 0x01, 0x00},
+		{0x00, 0x00, 0x00, 0x01},
+		{0x00, 0x00, 0x00, 0x00},
+	}
 	t.Run("Test One", func(t *testing.T) {
 		t.Parallel()
 
-		inputMatrix := []byte{0x05, 0x01, 0x00, 0x00}
+		// inputMatrix := []byte{0x05, 0x01, 0x00, 0x00}
 		expectedOutput := []byte{0x04, 0x01, 0x00, 0x00}
-		bitstring.ByteSubLE(inputMatrix)
-		
-		require.Equal(t, expectedOutput, inputMatrix)
+		bitstring.ByteSubLE(inputMatrix[0])
+
+		require.Equal(t, expectedOutput, inputMatrix[0])
 	})
 
 	t.Run("Test Two", func(t *testing.T) {
 		t.Parallel()
 
-		inputMatrix := []byte{0x00, 0x00, 0x01, 0x00}
+		// inputMatrix := []byte{0x00, 0x00, 0x01, 0x00}
 		expectedOutput := []byte{0xFF, 0xFF, 0x00, 0x00}
-		bitstring.ByteSubLE(inputMatrix)
-		
-		require.Equal(t, expectedOutput, inputMatrix)
+		bitstring.ByteSubLE(inputMatrix[1])
+
+		require.Equal(t, expectedOutput, inputMatrix[1])
 	})
 
 	t.Run("Test Three", func(t *testing.T) {
-		inputMatrix := []byte{0x00, 0x00, 0x00, 0x01}
+		// inputMatrix := []byte{0x00, 0x00, 0x00, 0x01}
 		expectedOutput := []byte{0xFF, 0xFF, 0xFF, 0x00}
-		bitstring.ByteSubLE(inputMatrix)
-		
-		require.Equal(t, expectedOutput, inputMatrix)
+		bitstring.ByteSubLE(inputMatrix[2])
+
+		require.Equal(t, expectedOutput, inputMatrix[2])
 	})
 
 	t.Run("Test Three", func(t *testing.T) {
 		t.Parallel()
 
-		inputMatrix := []byte{0x00, 0x00, 0x00, 0x00}
+		// inputMatrix := []byte{0x00, 0x00, 0x00, 0x00}
 		expectedOutput := []byte{0xFF, 0xFF, 0xFF, 0xFF}
-		bitstring.ByteSubLE(inputMatrix)
-		
-		require.Equal(t, expectedOutput, inputMatrix)
+		bitstring.ByteSubLE(inputMatrix[3])
+
+		require.Equal(t, expectedOutput, inputMatrix[3])
 	})
 }
 
 func TestToBytesLE(t *testing.T) {
-
+	inputInt := []int{
+		2147483647,
+		-2147483648,
+		0,
+	}
 	t.Run("Test for Positive int", func(t *testing.T) {
 		t.Parallel()
 
-		i := 123456789
-		expectedOutput := []byte {0x15, 0xCD, 0x5B, 0x07}
-		
-		result := bitstring.ToBytesLE(i)
+		// i := 123456789
+		expectedOutput := []byte{0x15, 0xCD, 0x5B, 0x07}
+
+		result := bitstring.ToBytesLE(inputInt[0])
 		require.Equal(t, expectedOutput, result)
 	})
 
 	t.Run("Test for Negative int", func(t *testing.T) {
 		t.Parallel()
 
-		i := -123456789
-		expectedOutput := []byte {0xEB, 0x32, 0xA4, 0xF8}
-		
-		result := bitstring.ToBytesLE(i)
+		// i := -123456789
+		expectedOutput := []byte{0xEB, 0x32, 0xA4, 0xF8}
+
+		result := bitstring.ToBytesLE(inputInt[1])
 		require.Equal(t, expectedOutput, result)
 	})
 
 	t.Run("Test for Zero", func(t *testing.T) {
 		t.Parallel()
 
-		i := 0
-        expectedOutput := []byte {0x00, 0x00, 0x00, 0x00}
-        
-        result := bitstring.ToBytesLE(i)
-        require.Equal(t, expectedOutput, result)
-	})
+		// i := 0
+		expectedOutput := []byte{0x00, 0x00, 0x00, 0x00}
 
-	t.Run("Test for max int32", func(t *testing.T) {
-		t.Parallel()
-
-		i := 2147483647
-		expectedOutput := []byte {0xFF, 0xFF, 0xFF, 0x7F}
-
-		result := bitstring.ToBytesLE(i)
-		require.Equal(t, expectedOutput, result)
-	})
-
-	t.Run("Test for max -int32", func(t *testing.T) {
-		t.Parallel()
-
-		i := -2147483648
-		expectedOutput := []byte {0x00, 0x00, 0x00, 0x80}
-
-		result := bitstring.ToBytesLE(i)
+		result := bitstring.ToBytesLE(inputInt[2])
 		require.Equal(t, expectedOutput, result)
 	})
 }
 
 func TestTruncateWithEllipsis(t *testing.T) {
 
+	inputText := []string{
+		"Hello",
+		"HelloWorld",
+		"Hello, World!",
+		"",
+	}
 	t.Run("maxLength being bigger than len(text)", func(t *testing.T) {
 		t.Parallel()
 
-		text:= "Hello"
+		// text := "Hello"
 		maxLength := 10
 		expectedOutput := "Hello"
-		result := bitstring.TruncateWithEllipsis(text, maxLength)
+		result := bitstring.TruncateWithEllipsis(inputText[0], maxLength)
 
 		require.Equal(t, expectedOutput, result)
 	})
@@ -306,102 +367,96 @@ func TestTruncateWithEllipsis(t *testing.T) {
 	t.Run("maxLength being equal to len(text)", func(t *testing.T) {
 		t.Parallel()
 
-		text:= "HelloWorld"
-        maxLength := 10
-        expectedOutput := "HelloWorld"
-        result := bitstring.TruncateWithEllipsis(text, maxLength)
+		// text := "HelloWorld"
+		maxLength := 10
+		expectedOutput := "HelloWorld"
+		result := bitstring.TruncateWithEllipsis(inputText[1], maxLength)
 
-        require.Equal(t, expectedOutput, result)
+		require.Equal(t, expectedOutput, result)
 	})
 
 	t.Run("maxLength being less than len(text)", func(t *testing.T) {
 		t.Parallel()
 
-		text:= "Hello, World!"
-        maxLength := 10
-        expectedOutput := "Hello, Wor...(3)"
-        result := bitstring.TruncateWithEllipsis(text, maxLength)
+		// text := "Hello, World!"
+		maxLength := 10
+		expectedOutput := "Hello, Wor...(3)"
+		result := bitstring.TruncateWithEllipsis(inputText[2], maxLength)
 
-        require.Equal(t, expectedOutput, result)
+		require.Equal(t, expectedOutput, result)
 	})
 
 	t.Run("empty string", func(t *testing.T) {
-		text:= ""
-        maxLength := 10
-        expectedOutput := ""
-        result := bitstring.TruncateWithEllipsis(text, maxLength)
+		// text := ""
+		maxLength := 10
+		expectedOutput := ""
+		result := bitstring.TruncateWithEllipsis(inputText[3], maxLength)
 
-        require.Equal(t, expectedOutput, result)
+		require.Equal(t, expectedOutput, result)
 	})
 
 	// t.Run("Test Five", func(t *testing.T) {
 	// 	text:= "Hello, World!"
-    //     maxLength := -1
-    //     expectedOutput := "Hello, World!"
-    //     result := bitstring.TruncateWithEllipsis(text, maxLength)
+	//     maxLength := -1
+	//     expectedOutput := "Hello, World!"
+	//     result := bitstring.TruncateWithEllipsis(text, maxLength)
 
-    //     require.Equal(t, expectedOutput, result)
+	//     require.Equal(t, expectedOutput, result)
 	// })
 }
 
-func TestMemclr (t *testing.T) {
-	//should I include unit,unit8,... test as well ?
-
-    t.Run("Test for matrix of type int", func(t *testing.T) {
+func TestMemclr(t *testing.T) {
+	inputMatrix := [][]int{
+		{1, 2, 3, 4},
+		{10, 20, 30, 40},
+		{100, 200, 300, 4000},
+		{1000000000, 2000000000, 300000000, 400000000},
+		{},
+	}
+	//test name to be changed
+	t.Run("Test for matrix of type int", func(t *testing.T) {
 		t.Parallel()
 
-        inputMatrix := []int{1, 2, 3, 4}
-        expectedOutput := []int{0, 0, 0, 0}
-        bitstring.Memclr(inputMatrix)
-        
-        require.Equal(t, expectedOutput, inputMatrix)
-    })
-
-	t.Run("Test for matrix of type int8", func(t *testing.T) {
-		t.Parallel()
-
-		inputMatrix := []int8{10, 20, 30, 40}
-		expectedOutput := []int8{0, 0, 0, 0}
-		bitstring.Memclr(inputMatrix)
+		// inputMatrix := []int{1, 2, 3, 4}
+		expectedOutput := []int{0, 0, 0, 0}
+		bitstring.Memclr(inputMatrix[0])
 
 		require.Equal(t, expectedOutput, inputMatrix)
 	})
+	t.Run("Test for matrix of type int8", func(t *testing.T) {
+		t.Parallel()
 
+		// inputMatrix := []int8{10, 20, 30, 40}
+		expectedOutput := []int8{0, 0, 0, 0}
+		bitstring.Memclr(inputMatrix[1])
+
+		require.Equal(t, expectedOutput, inputMatrix)
+	})
 	t.Run("Test for matrix of type int16", func(t *testing.T) {
 		t.Parallel()
 
-		inputMatrix := []int16{100, 200, 300, 4000}
+		// inputMatrix := []int16{100, 200, 300, 4000}
 		expectedOutput := []int16{0, 0, 0, 0}
-		bitstring.Memclr(inputMatrix)
+		bitstring.Memclr(inputMatrix[2])
 
 		require.Equal(t, expectedOutput, inputMatrix)
 	})
 	t.Run("Test for matrix of type int32", func(t *testing.T) {
 		t.Parallel()
 
-		inputMatrix := []int32{1000000000, 2000000000, 300000000, 400000000}
-        expectedOutput := []int32{0, 0, 0, 0}
-        bitstring.Memclr(inputMatrix)
+		// inputMatrix := []int32{1000000000, 2000000000, 300000000, 400000000}
+		expectedOutput := []int32{0, 0, 0, 0}
+		bitstring.Memclr(inputMatrix[3])
 
-        require.Equal(t, expectedOutput, inputMatrix)
+		require.Equal(t, expectedOutput, inputMatrix)
 	})
-	t.Run("Test for matrix of type int64", func(t *testing.T) {
-		t.Parallel()
-
-		inputMatrix := []int64{1000000000000000000, 2000000000000000000, 3000000000000000000, 4000000000000000000}
-        expectedOutput := []int64{0, 0, 0, 0}
-        bitstring.Memclr(inputMatrix)
-
-        require.Equal(t, expectedOutput, inputMatrix)
-	})
-
 	t.Run("Test for an empty matrix", func(t *testing.T) {
 		t.Parallel()
 
-		inputMatrix := []int{}
-        expectedOutput := []int{}
-        bitstring.Memclr(inputMatrix)
+		// inputMatrix := []int{}
+		expectedOutput := []int{}
+		bitstring.Memclr(inputMatrix[4])
 
-        require.Equal(t, expectedOutput, inputMatrix)
+		require.Equal(t, expectedOutput, inputMatrix)
 	})
 }
