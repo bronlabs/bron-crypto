b0f378c1d1c223b8aefd58b3a73233db4f3d18ac
mohammadsarraf <108194180+mohammadsarraf@users.noreply.github.com>
2024-04-22 07:06:10 -0400

    cleaned up the pr

--- pkg/hashing/hash.go
+++ pkg/hashing/hash.go
@@ -1,35 +1,73 @@
 package hashing
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"hash"
+	"io"
+	"slices"
 
+	"golang.org/x/crypto/hkdf"
+
+	"github.com/copperexchange/krypton-primitives/pkg/base/bitstring"
 	"github.com/copperexchange/krypton-primitives/pkg/base/errs"
 )
 
 // Hash iteratively writes all the inputs to the given hash function and returns the result.
 func Hash(h func() hash.Hash, xs ...[]byte) ([]byte, error) {
 	H := h()
-	for i, x := range xs {
+	for _, x := range xs {
 		if _, err := H.Write(x); err != nil {
-			return nil, errs.WrapFailed(err, "could not write to H for input %d", i)
+			return nil, errs.WrapFailed(err, "could not write to H")
 		}
 	}
 	digest := H.Sum(nil)
 	return digest, nil
 }
 
+// HashChain computes the HKDF over many values iteratively such that each
+// value is hashed separately and based on preceding values.
+//
+// The first value is computed as okm_0 = KDF(f || value) where
+//   - f is a byte slice of 32 0xFF
+//   - salt is zero-filled byte slice with length equal to the hash output length
+//   - info is the protocol name
+//   - okm is the 32 byte output
+//
+// The each subsequent iteration is computed by as okm_i = KDF(f_i || value || okm_{i-1})
+// where f_i = 2^b - 1 - i such that there are 0xFF bytes prior to the value.
+// f_1 changes the first byte to 0xFE, f_2 to 0xFD. The previous okm is appended to the value
+// to provide cryptographic domain separation.
+// See https://signal.org/docs/specifications/x3dh/#cryptographic-notation
+// and https://signal.org/docs/specifications/xeddsa/#hash-functions
+// for more details.
+// This uses the KDF function similar to X3DH for each `value`
+// But changes the key just like XEdDSA where the prefix bytes change by a single bit.
 func HashChain(h func() hash.Hash, xs ...[]byte) ([]byte, error) {
-	H := h()
-	var err error
-	out := make([]byte, H.Size())
-	for i, x := range xs {
-		out, err = Hash(h, out, x)
-		if err != nil {
-			return nil, errs.WrapHashing(err, "could not compute hash for input %d", i)
+	// Don't accept any nil arguments
+	for _, x := range xs {
+		if x == nil {
+			return nil, errs.NewIsNil("an input is nil")
 		}
 	}
-	return out, nil
+
+	info := []byte("COPPER_KRYPTON_HASH_CHAIN_HKDF-")
+	salt := make([]byte, 32)
+	okm := make([]byte, 32)
+	f := bytes.Repeat([]byte{0xFF}, 32)
+
+	for _, x := range xs {
+		ikm := slices.Concat(f, x)
+		ikm = append(ikm, okm...)
+		kdf := hkdf.New(h, ikm, salt, info)
+		_, err := io.ReadFull(kdf, okm)
+		if err != nil {
+			return nil, errs.WrapRandomSample(err, "write to kdf failed")
+		}
+
+		bitstring.ByteSubLE(f)
+	}
+	return okm, nil
 }
 
 // Hmac iteratively writes all the inputs to an hmac (defined by the hash function and the key) and returns the result.
