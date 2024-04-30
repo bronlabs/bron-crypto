55c03306248b45fdc93d18a338e14cf4ea38e774
Alexandre Adomnicai <alexandre.adomnicai@copper.co>
2024-04-30 09:55:25 +0200

    Extend tests coverage for hash-based commitments

--- pkg/comm/hashcomm/hashcomm_test.go
+++ pkg/comm/hashcomm/hashcomm_test.go
@@ -2,9 +2,16 @@ package hashcomm
 
 import (
 	crand "crypto/rand"
+	"encoding/hex"
 	"testing"
 
+	"github.com/copperexchange/krypton-primitives/pkg/base/errs"
 	"github.com/stretchr/testify/require"
+)
+
+var (
+	h   = CommitmentHashFunction
+	sid = []byte("00000001")
 )
 
 func TestHappyPath(t *testing.T) {
@@ -24,33 +31,36 @@ func TestHappyPath(t *testing.T) {
 // An entry into our test table
 type entry struct {
 	// Input
-	message    Message
-	opening    *Opening
-	commitment *Commitment
-	err        error
+	msg Message
+	// Output
+	opn *Opening
+	com *Commitment
+	err error
 }
 
 func getEntries() []entry {
 	var testResults = []entry{
-		{message: []byte("This is a test message"), opening: nil, commitment: nil, err: nil},
-		// {opening: NewOpening([]byte("short msg"), nil), commitment: nil, err: nil},
-		// {
-		// 	opening:    NewOpening([]byte("This input field is intentionally longer than the SHA256 block size to ensure that the entire message is processed"), nil),
-		// 	commitment: NewCommitment(nil), err: nil,
-		// },
-		// {
-		// 	opening:    NewOpening([]byte{0xFB, 0x1A, 0x18, 0x47, 0x39, 0x3C, 0x9F, 0x45, 0x5F, 0x29, 0x4C, 0x51, 0x42, 0x30, 0xA6, 0xB9}, nil),
-		// 	commitment: NewCommitment(nil), err: nil,
-		// },
-		// // msg = \epsilon (empty string)
-		// {opening: NewOpening([]byte{}, nil), commitment: NewCommitment(nil), err: nil},
-		// // msg == nil
-		// {opening: NewOpening(nil, nil), commitment: NewCommitment(nil), err: nil},
+		{msg: []byte("This is a test message"), opn: nil, com: nil, err: nil},
+		{msg: []byte("short msg"), opn: nil, com: nil, err: nil},
+		{
+			msg: []byte(`This input field is intentionally longer than the SHA256 block size and the largest
+			rate of all SHA3 variants as defined in NIST FIPS PUB 202 (i.e. r = 1152 bits = 144 bytes for SHA3-224) 
+			to cover cases where multiple blocks have to be processed.`),
+			opn: nil, com: nil, err: nil,
+		},
+		{
+			msg: []byte{0xFB, 0x1A, 0x18, 0x47, 0x39, 0x3C, 0x9F, 0x45, 0x5F, 0x29, 0x4C, 0x51, 0x42, 0x30, 0xA6, 0xB9},
+			opn: nil, com: nil, err: nil,
+		},
+		// msg = \epsilon (empty string)
+		{msg: []byte{}, opn: nil, com: nil, err: nil},
+		// msg == nil
+		{msg: nil, opn: nil, com: nil, err: nil},
 	}
 	for i := range testResults {
-		c := NewCommitter([]byte("00000001"))
+		c := NewCommitter(sid)
 		entry := &testResults[i]
-		entry.commitment, entry.opening, entry.err = c.Commit(crand.Reader, entry.message)
+		entry.com, entry.opn, entry.err = c.Commit(crand.Reader, entry.msg)
 	}
 	return testResults
 }
@@ -64,248 +74,255 @@ func TestCommitWithoutErrors(t *testing.T) {
 	}
 }
 
-// // Commitments should be 256b == 64B in length
-// func TestCommitmentsAreExpectedLength(t *testing.T) {
-// 	t.Parallel()
-// 	testResults := getEntries()
-// 	expLen := h().Size()
-// 	for _, entry := range testResults {
-// 		require.Lenf(t, entry.commit, expLen, "commitment is not expected length: %v != %v", len(entry.commit), expLen)
-// 	}
-// }
+// Commitments should be 256b == 64B in length
+func TestCommitmentsAreExpectedLength(t *testing.T) {
+	t.Parallel()
+	testResults := getEntries()
+	expLen := h().Size()
+	for _, entry := range testResults {
+		require.Lenf(t, entry.com.commitment, expLen, "commitment is not expected length: %v != %v", len(entry.com.commitment), expLen)
+	}
+}
 
-// // Decommit cannot be nil
-// func TestCommmitProducesDecommit(t *testing.T) {
-// 	t.Parallel()
-// 	testResults := getEntries()
-// 	for _, entry := range testResults {
-// 		require.NotNilf(t, entry.witness, "decommit cannot be nil: Commit(%v)", entry.msg)
-// 	}
-// }
+// Decommit cannot be nil
+func TestCommmitProducesDecommit(t *testing.T) {
+	t.Parallel()
+	testResults := getEntries()
+	for _, entry := range testResults {
+		require.NotNilf(t, entry.opn.witness, "decommit cannot be nil: Commit(%v)", entry.msg)
+	}
+}
 
-// // Commitments should be unique
-// func TestCommmitProducesDistinctCommitments(t *testing.T) {
-// 	t.Parallel()
-// 	seen := make(map[string]bool)
-// 	testResults := getEntries()
+// Commitments should be unique
+func TestCommmitProducesDistinctCommitments(t *testing.T) {
+	t.Parallel()
+	seen := make(map[string]bool)
+	testResults := getEntries()
 
-// 	// Check the pre-computed commitments for uniquness
-// 	for _, entry := range testResults {
-// 		// Slices cannot be used as hash keys, so we need to copy into
-// 		// an array. Oh, go-lang.
-// 		cee := make([]byte, h().Size())
-// 		copy(cee[:], entry.commit)
+	// Check the pre-computed commitments for uniquness
+	for _, entry := range testResults {
+		// Slices cannot be used as hash keys, so we need to copy into
+		// an array. Oh, go-lang.
+		cee := make([]byte, h().Size())
+		copy(cee[:], entry.com.commitment)
 
-// 		serialised := hex.EncodeToString(cee)
-// 		// Ensure each commit is unique
-// 		require.NotContainsf(t, seen, serialised, "duplicate commit found: %v", cee)
-// 		seen[serialised] = true
-// 	}
-// }
+		serialised := hex.EncodeToString(cee)
+		// Ensure each commit is unique
+		require.NotContainsf(t, seen, serialised, "duplicate commit found: %v", cee)
+		seen[serialised] = true
+	}
+}
 
-// // Commitments should be unique even for the same message since the nonce is
-// // randomly selected
-// func TestCommmitDistinctCommitments(t *testing.T) {
-// 	t.Parallel()
-// 	seen := make(map[string]bool)
-// 	msg := []byte("black lives matter")
-// 	const iterations = 1000
+// Commitments should be unique even for the same message since the nonce is
+// randomly selected
+func TestCommmitDistinctCommitments(t *testing.T) {
+	t.Parallel()
+	c := NewCommitter(sid)
+	seen := make(map[string]bool)
+	msg := []byte("black lives matter")
+	const iterations = 1000
 
-// 	// Check the pre-computed commitments for uniquness
-// 	for i := 0; i < iterations; i++ {
-// 		// Compute a commitment
-// 		c, _, err := commitments.CommitWithoutSession(crand.Reader, msg)
-// 		if err != nil {
-// 			t.Error(err)
-// 		}
+	// Check the pre-computed commitments for uniquness
+	for i := 0; i < iterations; i++ {
+		// Compute a commitment
+		com, _, err := c.Commit(crand.Reader, msg)
+		if err != nil {
+			t.Error(err)
+		}
 
-// 		// Slices cannot be used as hash keys, so copy into an array
-// 		cee := make([]byte, h().Size())
-// 		copy(cee[:], []byte(c))
+		// Slices cannot be used as hash keys, so copy into an array
+		cee := make([]byte, h().Size())
+		copy(cee[:], []byte(com.commitment))
 
-// 		serialised := hex.EncodeToString(cee)
+		serialised := hex.EncodeToString(cee)
 
-// 		// Ensure each commit is unique
-// 		require.NotContainsf(t, seen, serialised, "duplicate commit found: %v", cee)
-// 		seen[serialised] = true
-// 	}
-// }
+		// Ensure each commit is unique
+		require.NotContainsf(t, seen, serialised, "duplicate commit found: %v", cee)
+		seen[serialised] = true
+	}
+}
 
-// // Nonces must be 256b = 64B
-// func TestCommmitNonceIsExpectedLength(t *testing.T) {
-// 	t.Parallel()
-// 	expLen := h().Size()
-// 	testResults := getEntries()
+// Nonces must be 256b = 64B
+func TestCommmitNonceIsExpectedLength(t *testing.T) {
+	t.Parallel()
+	expLen := h().Size()
+	testResults := getEntries()
 
-// 	// Check the pre-computed nonces
-// 	for _, entry := range testResults {
-// 		require.Lenf(t, entry.witness, expLen, "nonce is not expected length: %v != %v", len(entry.witness), expLen)
-// 	}
-// }
+	// Check the pre-computed nonces
+	for _, entry := range testResults {
+		require.Lenf(t, entry.opn.witness, expLen, "nonce has not expected length: %v != %v", len(entry.opn.witness), expLen)
+	}
+}
 
-// // Randomly selected nonces will be unique with overwhelming probability
-// func TestCommmitProducesDistinctNonces(t *testing.T) {
-// 	t.Parallel()
-// 	seen := make(map[string]bool)
-// 	msg := []byte("black lives matter")
-// 	const iterations = 1000
+// Randomly selected nonces will be unique with overwhelming probability
+func TestCommmitProducesDistinctNonces(t *testing.T) {
+	t.Parallel()
+	c := NewCommitter(sid)
+	seen := make(map[string]bool)
+	msg := []byte("black lives matter")
+	const iterations = 1000
 
-// 	// Check the pre-computed commitments for uniquness
-// 	for i := 0; i < iterations; i++ {
-// 		// Compute a commitment
-// 		_, dee, err := commitments.CommitWithoutSession(crand.Reader, msg)
-// 		require.NoError(t, err)
+	// Check the pre-computed commitments for uniquness
+	for i := 0; i < iterations; i++ {
+		// Compute a commitment
+		_, opn, err := c.Commit(crand.Reader, msg)
+		require.NoError(t, err)
 
-// 		// Ensure each nonce is unique
-// 		serialised := hex.EncodeToString(dee)
-// 		require.NotContainsf(t, seen, serialised, "duplicate nonce found: %v", dee)
-// 		seen[serialised] = true
-// 	}
-// }
+		// Ensure each nonce is unique
+		serialised := hex.EncodeToString(opn.witness)
+		require.NotContainsf(t, seen, serialised, "duplicate nonce found: %v", opn.witness)
+		seen[serialised] = true
+	}
+}
 
-// func TestOpenOnValidCommitments(t *testing.T) {
-// 	testResults := getEntries()
-// 	for _, entry := range testResults {
-// 		// OpenWithSession each commitment
-// 		err := commitments.OpenWithoutSession(entry.commit, entry.witness, entry.msg)
-// 		// There should be no error
-// 		require.NoErrorf(t, err, "commitment of message failed: %s", entry.msg)
-// 	}
-// }
+func TestOpenOnValidCommitments(t *testing.T) {
+	testResults := getEntries()
+	v := NewVerifier(sid)
+	for _, entry := range testResults {
+		// Verify each commitment
+		err := v.Verify(entry.com, entry.opn)
+		// There should be no error
+		require.NoErrorf(t, err, "commitment of message failed: %s", entry.msg)
+	}
+}
 
-// func TestOpenOnModifiedNonce(t *testing.T) {
-// 	t.Parallel()
-// 	testResults := getEntries()
-// 	for _, entry := range testResults {
-// 		dPrime := entry.witness[:]
+func TestOpenOnModifiedNonce(t *testing.T) {
+	t.Parallel()
+	v := NewVerifier(sid)
+	testResults := getEntries()
+	for _, entry := range testResults {
+		localOpening := entry.opn
+		// Modify the nonce MSB
+		localOpening.witness[0] ^= 0x80
+		// Verify and check for failure
+		err := v.Verify(entry.com, localOpening)
+		require.True(t, errs.IsVerification(err))
+	}
+}
 
-// 		// Modify the nonce
-// 		dPrime[0] ^= 0x40
+func TestOpenOnZeroPrefixNonce(t *testing.T) {
+	t.Parallel()
+	v := NewVerifier(sid)
+	testResults := getEntries()
+	for _, entry := range testResults {
+		localOpening := entry.opn
+		// Modify the nonce
+		localOpening.witness[0] = 0x00
+		localOpening.witness[1] = 0x00
+		localOpening.witness[2] = 0x00
+		localOpening.witness[3] = 0x00
+		localOpening.witness[4] = 0x00
+		localOpening.witness[5] = 0x00
+		localOpening.witness[6] = 0x00
+		localOpening.witness[7] = 0x00
+		localOpening.witness[8] = 0x00
+		localOpening.witness[9] = 0x00
+		localOpening.witness[10] = 0x00
+		// Verify and check for failure
+		err := v.Verify(entry.com, localOpening)
+		require.True(t, errs.IsVerification(err))
+	}
+}
 
-// 		// OpenWithSession and check for failure
-// 		err := commitments.OpenWithoutSession(entry.commit, dPrime, entry.msg)
-// 		require.True(t, errs.IsVerification(err))
-// 	}
-// }
+// An unrelated message should fail on open
+func TestOpenOnNewMessage(t *testing.T) {
+	t.Parallel()
+	v := NewVerifier(sid)
+	testResults := getEntries()
+	for _, entry := range testResults {
+		localOpening := entry.opn
+		// Use a distinct message
+		localOpening.message = []byte("no one expects the spanish inquisition")
+		// Verify and check for failure
+		err := v.Verify(entry.com, localOpening)
+		require.True(t, errs.IsVerification(err))
+	}
+}
 
-// func TestOpenOnZeroPrefixNonce(t *testing.T) {
-// 	t.Parallel()
-// 	testResults := getEntries()
-// 	for _, entry := range testResults {
-// 		dPrime := entry.witness[:]
+// A modified message should fail on open
+func TestOpenOnModifiedMessage(t *testing.T) {
+	t.Parallel()
+	v := NewVerifier(sid)
+	testResults := getEntries()
+	localTestResults := testResults
+	for _, entry := range localTestResults {
+		// Skip the empty string message for this test case
+		if len(entry.msg) == 0 {
+			continue
+		}
+		localOpening := entry.opn
+		// Modify the message LSB
+		localOpening.message[0] ^= 0x01
+		// Verify and check for failure
+		err := v.Verify(entry.com, localOpening)
+		require.True(t, errs.IsVerification(err))
+	}
+}
 
-// 		// Modify the nonce
-// 		dPrime[0] = 0x00
-// 		dPrime[1] = 0x00
-// 		dPrime[2] = 0x00
-// 		dPrime[3] = 0x00
-// 		dPrime[4] = 0x00
-// 		dPrime[5] = 0x00
-// 		dPrime[6] = 0x00
-// 		dPrime[7] = 0x00
-// 		dPrime[8] = 0x00
-// 		dPrime[9] = 0x00
-// 		dPrime[10] = 0x00
+// A modified commitment should fail on open
+func TestOpenOnModifiedCommitment(t *testing.T) {
+	t.Parallel()
+	v := NewVerifier(sid)
+	testResults := getEntries()
+	for _, entry := range testResults {
+		// Copy and then modify the commitment
+		localCommitment := entry.com
+		localCommitment.commitment[6] ^= 0x33
+		// Verify and check for failure
+		err := v.Verify(localCommitment, entry.opn)
+		require.True(t, errs.IsVerification(err))
+	}
+}
 
-// 		// OpenWithSession and check for failure
-// 		err := commitments.OpenWithoutSession(entry.commit, dPrime, entry.msg)
-// 		require.True(t, errs.IsVerification(err))
-// 	}
-// }
+// An empty decommit should fail to open
+func TestOpenOnDefaultDecommitObject(t *testing.T) {
+	t.Parallel()
+	v := NewVerifier(sid)
+	testResults := getEntries()
+	for _, entry := range testResults {
+		localOpening := entry.opn
+		localOpening.witness = nil
+		// OpenWithSession and check for failure
+		err := v.Verify(entry.com, localOpening)
+		require.True(t, errs.IsArgument(err))
+	}
+}
 
-// // An unrelated message should fail on open
-// func TestOpenOnNewMessage(t *testing.T) {
-// 	t.Parallel()
-// 	testResults := getEntries()
-// 	for _, entry := range testResults {
-// 		dPrime := entry.witness[:]
+// A nil commit should return an error
+func TestOpenOnNilCommitment(t *testing.T) {
+	t.Parallel()
+	v := NewVerifier(sid)
+	testResults := getEntries()
+	for _, entry := range testResults {
+		err := v.Verify(NewCommitment([]byte{}), entry.opn)
+		require.True(t, errs.IsArgument(err))
+	}
+}
 
-// 		// Use a distinct message
-// 		msg := []byte("no one expects the spanish inquisition")
+// Too long commitment should produce an error
+func TestOpenOnLongCommitment(t *testing.T) {
+	t.Parallel()
+	v := NewVerifier(sid)
+	testResults := getEntries()
+	for _, entry := range testResults {
+		localCommitment := NewCommitment(make([]byte, h().Size()+1))
+		copy(localCommitment.commitment, entry.com.commitment)
+		// OpenWithSession and check for failure
+		err := v.Verify(localCommitment, entry.opn)
+		require.True(t, errs.IsArgument(err))
+	}
+}
 
-// 		// OpenWithSession and check for failure
-// 		err := commitments.OpenWithoutSession(entry.commit, dPrime, msg)
-// 		require.True(t, errs.IsVerification(err))
-// 	}
-// }
-
-// // A modified message should fail on open
-// func TestOpenOnModifiedMessage(t *testing.T) {
-// 	t.Parallel()
-// 	testResults := getEntries()
-// 	localTestResults := testResults
-// 	for _, entry := range localTestResults {
-// 		// Skip the empty string message for this test case
-// 		if len(entry.msg) == 0 {
-// 			continue
-// 		}
-
-// 		// Modify the message _in situ_
-// 		dPrime := entry.witness[:]
-// 		dPrime[1] ^= 0x99
-
-// 		// OpenWithSession and check for failure
-// 		err := commitments.OpenWithoutSession(entry.commit, dPrime, entry.msg)
-// 		require.True(t, errs.IsVerification(err))
-// 	}
-// }
-
-// // A modified commitment should fail on open
-// func TestOpenOnModifiedCommitment(t *testing.T) {
-// 	t.Parallel()
-// 	testResults := getEntries()
-// 	for _, entry := range testResults {
-// 		// Copy and then modify the commitment
-// 		cPrime := make([]byte, h().Size())
-// 		copy(cPrime[:], entry.commit)
-// 		cPrime[6] ^= 0x33
-
-// 		// OpenWithSession and check for failure
-// 		err := commitments.OpenWithoutSession(cPrime, entry.witness, entry.msg)
-// 		require.True(t, errs.IsVerification(err))
-// 	}
-// }
-
-// // An empty decommit should fail to open
-// func TestOpenOnDefaultDecommitObject(t *testing.T) {
-// 	t.Parallel()
-// 	testResults := getEntries()
-// 	for _, entry := range testResults {
-// 		// OpenWithSession and check for failure
-// 		err := commitments.OpenWithoutSession(entry.commit, commitments.Witness{}, entry.msg)
-// 		require.True(t, errs.IsArgument(err))
-// 	}
-// }
-
-// // A nil commit should return an error
-// func TestOpenOnNilCommitment(t *testing.T) {
-// 	t.Parallel()
-// 	err := commitments.OpenWithoutSession(nil, commitments.Witness{}, nil)
-// 	require.True(t, errs.IsArgument(err))
-// }
-
-// // Too long commitment should produce an error
-// func TestOpenOnLongCommitment(t *testing.T) {
-// 	t.Parallel()
-// 	testResults := getEntries()
-// 	for _, entry := range testResults {
-// 		tooLong := make([]byte, h().Size()+1)
-// 		copy(tooLong, entry.msg)
-// 		// OpenWithSession and check for failure
-// 		err := commitments.OpenWithoutSession(tooLong, entry.witness, entry.msg)
-// 		require.True(t, errs.IsArgument(err))
-// 	}
-// }
-
-// // Too short commitment should produce an error
-// func TestOpenOnShortCommitment(t *testing.T) {
-// 	t.Parallel()
-// 	testResults := getEntries()
-// 	for _, entry := range testResults {
-// 		tooShort := make([]byte, h().Size()-1)
-// 		copy(tooShort, entry.msg)
-// 		// OpenWithSession and check for failure
-// 		err := commitments.OpenWithoutSession(tooShort, entry.witness, entry.msg)
-// 		require.True(t, errs.IsArgument(err))
-// 	}
-// }
+// Too short commitment should produce an error
+func TestOpenOnShortCommitment(t *testing.T) {
+	t.Parallel()
+	v := NewVerifier(sid)
+	testResults := getEntries()
+	for _, entry := range testResults {
+		localCommitment := NewCommitment(make([]byte, h().Size()-1))
+		copy(localCommitment.commitment, entry.com.commitment)
+		// OpenWithSession and check for failure
+		err := v.Verify(localCommitment, entry.opn)
+		require.True(t, errs.IsArgument(err))
+	}
+}
