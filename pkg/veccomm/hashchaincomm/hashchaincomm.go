ddc1ba8fef0a0d41c9f26445fcf2aedb5fe33cab
Alexandre Adomnicai <alexandre.adomnicai@copper.co>
2024-05-03 17:42:49 +0200

    Take advantage of inherited interface

--- pkg/veccomm/hashchaincomm/hashchaincomm.go
+++ pkg/veccomm/hashchaincomm/hashchaincomm.go
@@ -1,27 +1,24 @@
 package hashchaincomm
 
 import (
-	"crypto/subtle"
+	"bytes"
 	"io"
 	"slices"
 
-	"github.com/copperexchange/krypton-primitives/pkg/base"
 	"github.com/copperexchange/krypton-primitives/pkg/base/bitstring"
 	"github.com/copperexchange/krypton-primitives/pkg/base/errs"
 	"github.com/copperexchange/krypton-primitives/pkg/comm/hashcomm"
-	"github.com/copperexchange/krypton-primitives/pkg/hashing"
 	"github.com/copperexchange/krypton-primitives/pkg/veccomm"
 )
 
 const Name = "HASHCHAIN_COMMITMENT"
 
 type VectorCommitter struct {
-	prng      io.Reader
-	sessionId []byte
+	committer *hashcomm.Committer
 }
 
 type VectorVerifier struct {
-	sessionId []byte
+	verifier *hashcomm.Verifier
 }
 
 type VectorCommitment struct {
@@ -30,56 +27,55 @@ type VectorCommitment struct {
 }
 
 type Opening struct {
+	opening hashcomm.Opening
 	Vector_ veccomm.Vector[hashcomm.Message]
-	Witness hashcomm.Witness
 }
 
 // not UC-secure without session-id
 func NewVectorCommitter(prng io.Reader, sessionId []byte) (*VectorCommitter, error) {
-	if prng == nil {
-		return nil, errs.NewIsNil("prng is nil")
+	committer, err := hashcomm.NewCommitter(prng, sessionId)
+	if err != nil {
+		return nil, errs.WrapFailed(err, "could not instantiate a committer")
 	}
-	return &VectorCommitter{prng, sessionId}, nil
+	return &VectorCommitter{committer}, nil
 }
 
 // not UC-secure without session-id
 func NewVectorVerifier(sessionId []byte) (*VectorVerifier, error) {
-	return &VectorVerifier{sessionId}, nil
+	committer, err := hashcomm.NewVerifier(sessionId)
+	if err != nil {
+		return nil, errs.WrapFailed(err, "could not instantiate a verifier")
+	}
+	return &VectorVerifier{committer}, nil
 }
 
 func encode(msg []byte, i int) []byte {
 	return slices.Concat(bitstring.ToBytes32LE(int32(i)), bitstring.ToBytes32LE(int32(len(msg))), msg)
 }
 
-func encodeWithSessionId(sessionId []byte, vector veccomm.Vector[hashcomm.Message]) [][]byte {
-	encoded := make([][]byte, len(vector)+1)
-	encoded[0] = slices.Concat([]byte("SESSION_ID_"), bitstring.ToBytes32LE(int32(len(sessionId))), sessionId)
+func chainEncodingVector(vector veccomm.Vector[hashcomm.Message]) hashcomm.Message {
+	encoded := make([][]byte, len(vector))
 	for i, m := range vector {
-		encoded[i+1] = encode(m, i)
+		encoded[i] = encode(m, i)
+		if i > 0 {
+			encoded[i] = append(encoded[i-1], encoded[i]...)
+		}
 	}
-	return encoded
+	return bytes.Join(encoded, nil)
 }
 
 func (c *VectorCommitter) Commit(vector veccomm.Vector[hashcomm.Message]) (*VectorCommitment, *Opening, error) {
-	witness := make([]byte, base.CollisionResistanceBytes)
-	if _, err := io.ReadFull(c.prng, witness); err != nil {
-		return nil, nil, errs.WrapRandomSample(err, "reading random bytes")
-	}
-	commitment, err := hashing.HmacChain(witness, hashcomm.CommitmentHashFunction, encodeWithSessionId(c.sessionId, vector)...)
+	commitment, opening, err := c.committer.Commit(chainEncodingVector(vector))
 	if err != nil {
 		return nil, nil, errs.WrapFailed(err, "could not compute commitment")
 	}
-	return &VectorCommitment{hashcomm.Commitment{Commitment: commitment}, uint(len(vector))}, &Opening{Vector_: vector, Witness: witness}, nil
+	return &VectorCommitment{*commitment, uint(len(vector))}, &Opening{*opening, vector}, nil
 }
 
 func (v *VectorVerifier) Verify(veccom *VectorCommitment, opening *Opening) error {
-	localCommitment, err := hashing.HmacChain(opening.Witness, hashcomm.CommitmentHashFunction, encodeWithSessionId(v.sessionId, opening.Vector_)...)
+	err := v.verifier.Verify(&veccom.commitment, &opening.opening)
 	if err != nil {
-		return errs.WrapFailed(err, "could not recompute the commitment")
-	}
-	if subtle.ConstantTimeCompare(veccom.commitment.Commitment, localCommitment) != 1 {
 		return errs.NewVerification("verification failed")
 	}
 	return nil
-
 }
