b85ebbb663596284e998f9aad4cd99b5e01c34a4
Andrey Pshenkin <andrey.pshenkin@gmail.com>
2024-07-15 14:21:40 +0100

    add make to compile boringssl for ios (#368)

--- thirdparty/thirdparty.mk
+++ thirdparty/thirdparty.mk
@@ -1,13 +1,17 @@
 BORINGSSL_SUBMODULE := $(KRYPTON_PRIMITIVES_THIRD_PARTY_DIR)/boringssl
 BORINGSSL_BUILD:= $(BORINGSSL_SUBMODULE)/build
+BORINGSSL_BUILD_ARGS := ""
 
 ${BORINGSSL_SUBMODULE}/include/openssl/bn.h:
 	git submodule update --init --recursive ${BORINGSSL_SUBMODULE}
 
 # might not work on windows
 ${BORINGSSL_BUILD}/crypto/libcrypto.a: ${BORINGSSL_SUBMODULE}/include/openssl/bn.h
-	cmake ${BORINGSSL_SUBMODULE} -DCMAKE_BUILD_TYPE=Release -DOPENSSL_SMALL=1 -GNinja -B ${BORINGSSL_BUILD}
+	cmake ${BORINGSSL_SUBMODULE} -DCMAKE_BUILD_TYPE=Release ${BORINGSSL_BUILD_ARGS} -DOPENSSL_SMALL=1 -GNinja -B ${BORINGSSL_BUILD}
 	ninja -C ${BORINGSSL_BUILD} -j7 crypto
 
 .PHONY: deps-boring
 deps-boring: ${BORINGSSL_BUILD}/crypto/libcrypto.a
+
+deps-boring-ios:
+	make deps-boring BORINGSSL_BUILD_ARGS="-DCMAKE_OSX_SYSROOT=${sysroot} -DCMAKE_OSX_ARCHITECTURES=arm64"
