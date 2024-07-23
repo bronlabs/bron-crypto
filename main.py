56f2496f6fe747792d1dac642c74827245034984
mohammadsarraf <108194180+mohammadsarraf@users.noreply.github.com>
2024-07-23 04:28:11 -0400

    script update

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import subprocess
 
-# linters = ["err113" "exhaustive" "exportloopref" forbidigo forcetypeassert gci gocheckcompilerdirectives gochecknoinits gochecksumtype goconst gocritic godot gofmt gomoddirectives goprintffuncname gosec gosimple gosmopolitan govet importas inamedparam ineffassign loggercheck makezero mirror misspell nakedret nilerr nilnil noctx nolintlint nosprintfhostport prealloc predeclared promlinter revive rowserrcheck sqlclosecheck staticcheck tagalign tagliatelle tenv testableexamples testifylint thelper tparallel unconvert unparam unused usestdlibvars wastedassign whitespace wrapcheck zerologlint]
-
 linters = ["asasalint", "asciicheck", "bidichk", "bodyclose", "containedctx", "contextcheck", "copyloopvar", "depguard", "dogsled", "dupl", "dupword", "durationcheck", "err113", "errcheck", "errchkjson", "errname", "errorlint", "exhaustive", "exportloopref", "forbidigo", "forcetypeassert", "gci", "gocheckcompilerdirectives", "gochecknoinits", "gochecksumtype", "goconst", "gocritic", "godot", "gofmt", "gomoddirectives", "goprintffuncname", "gosec", "gosimple", "gosmopolitan", "govet", "importas", "inamedparam", "ineffassign", "loggercheck", "makezero", "mirror", "misspell", "nakedret", "nilerr", "nilnil", "noctx", "nolintlint", "nosprintfhostport", "prealloc", "predeclared", "promlinter", "revive", "rowserrcheck", "sqlclosecheck", "staticcheck", "tagalign", "tagliatelle", "tenv", "testableexamples", "testifylint", "thelper", "tparallel", "unconvert", "unparam", "unused", "usestdlibvars", "wastedassign", "whitespace", "wrapcheck", "zerologlint"] 
 
 cmd = "golangci-lint cache clean &&  golangci-lint run --verbose --timeout=120m --print-resources-usage --disable-all --enable="
@@ -20,7 +18,7 @@ def runDebugLinters(file):
 
 def runLinters(linterToRun, file, i):
     linters_str = ",".join(linterToRun)
-    cmd = f"golangci-lint cache clean &&  golangci-lint run --verbose --timeout=120m --print-resources-usage --disable-all --enable={linters_str}"
+    cmd = f"golangci-lint cache clean &&  golangci-lint run --verbose --timeout=240m --print-resources-usage --disable-all --enable={linters_str}"
     print(str(i) + "\n"+ str(linterToRun))
     # run cmd
     result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
