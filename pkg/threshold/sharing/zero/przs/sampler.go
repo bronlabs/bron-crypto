316221ac83ef63cab9e95880d1115ffd1803c883
Mateusz Kramarczyk <mateusz.kramarczyk@bron.xyz>
2025-08-12 15:03:56 +0200

    zero sharing

---
+++ pkg/threshold/sharing/zero/przs/sampler.go
@@ -0,0 +1,66 @@
+package przs
+
+import (
+	"io"
+	"math/rand/v2"
+
+	"github.com/bronlabs/bron-crypto/pkg/base/algebra"
+	ds "github.com/bronlabs/bron-crypto/pkg/base/datastructures"
+	"github.com/bronlabs/bron-crypto/pkg/base/datastructures/hashmap"
+	"github.com/bronlabs/bron-crypto/pkg/base/errs"
+	"github.com/bronlabs/bron-crypto/pkg/network"
+	"github.com/bronlabs/bron-crypto/pkg/threshold/sharing"
+)
+
+type Sampler[FE algebra.PrimeFieldElement[FE]] struct {
+	field       algebra.PrimeField[FE]
+	mySharingId sharing.ID
+	seededPrngs ds.Map[sharing.ID, io.Reader]
+}
+
+func NewSampler[FE algebra.PrimeFieldElement[FE]](sharingId sharing.ID, quorum network.Quorum, seeds Seeds, field algebra.PrimeField[FE]) (*Sampler[FE], error) {
+	// TODO: add validation
+	prngs := hashmap.NewComparable[sharing.ID, io.Reader]()
+	for id := range quorum.Iter() {
+		if id == sharingId {
+			continue
+		}
+		seed, ok := seeds.Get(id)
+		if !ok {
+			return nil, errs.NewValidation("missing seed for %d", id)
+		}
+
+		prng := rand.NewChaCha8(seed)
+		prngs.Put(id, prng)
+	}
+	p := &Sampler[FE]{
+		field:       field,
+		mySharingId: sharingId,
+		seededPrngs: prngs.Freeze(),
+	}
+
+	return p, nil
+}
+
+func (s *Sampler[FE]) Sample() (FE, error) {
+	var nilFE FE
+	share := s.field.Zero()
+
+	for id, prng := range s.seededPrngs.Iter() {
+		sample, err := s.field.Random(prng)
+		if err != nil {
+			return nilFE, errs.WrapRandomSample(err, "could not sample scalar")
+		}
+
+		if id < s.mySharingId {
+			share = share.Add(sample)
+		} else {
+			share = share.Add(sample.Neg())
+		}
+	}
+
+	if share.IsZero() {
+		return nilFE, errs.NewFailed("could not sample a zero share")
+	}
+	return share, nil
+}
---
+++ pkg/threshold/sharing/zero/przs/setup/rounds.go
@@ -0,0 +1,117 @@
+package przsSetup
+
+import (
+	"crypto/subtle"
+	"io"
+
+	ds "github.com/bronlabs/bron-crypto/pkg/base/datastructures"
+	"github.com/bronlabs/bron-crypto/pkg/base/datastructures/hashmap"
+	"github.com/bronlabs/bron-crypto/pkg/base/errs"
+	hash_comm "github.com/bronlabs/bron-crypto/pkg/commitments/hash"
+	"github.com/bronlabs/bron-crypto/pkg/network"
+	"github.com/bronlabs/bron-crypto/pkg/threshold/sharing"
+	"github.com/bronlabs/bron-crypto/pkg/threshold/sharing/zero/przs"
+)
+
+func (p *Participant) Round1() (*Round1Broadcast, error) {
+	seedContributions := hashmap.NewComparable[sharing.ID, [32]byte]()
+	commitments := hashmap.NewComparable[sharing.ID, hash_comm.Commitment]()
+	witnesses := hashmap.NewComparable[sharing.ID, hash_comm.Witness]()
+	for sharingId := range p.quorum.Iter() {
+		if sharingId == p.mySharingId {
+			continue
+		}
+
+		var seedContribution [32]byte
+		if _, err := io.ReadFull(p.prng, seedContribution[:]); err != nil {
+			return nil, errs.WrapRandomSample(err, "cannot sample seed contribution")
+		}
+
+		seedContributionCommitment, seedContributionWitness, err := p.state.commitmentScheme.Committer().Commit(seedContribution[:], p.prng)
+		if err != nil {
+			return nil, errs.WrapFailed(err, "cannot commit seed contribution")
+		}
+
+		seedContributions.Put(sharingId, seedContribution)
+		commitments.Put(sharingId, seedContributionCommitment)
+		witnesses.Put(sharingId, seedContributionWitness)
+	}
+
+	p.state.seedContributions = seedContributions.Freeze()
+	p.state.witnesses = witnesses.Freeze()
+	p.state.commitments = hashmap.NewComparable[sharing.ID, ds.Map[sharing.ID, hash_comm.Commitment]]()
+	p.state.commitments.Put(p.mySharingId, commitments.Freeze())
+	r1bo := &Round1Broadcast{commitments: commitments.Freeze()}
+	return r1bo, nil
+}
+
+func (p *Participant) Round2(r1bo network.RoundMessages[*Round1Broadcast]) (network.RoundMessages[*Round2P2P], error) {
+	for sharingId := range p.quorum.Iter() {
+		if sharingId == p.mySharingId {
+			continue
+		}
+
+		msg, ok := r1bo.Get(sharingId)
+		if !ok {
+			return nil, errs.NewValidation("missing message from %d", sharingId)
+		}
+		p.state.commitments.Put(sharingId, msg.commitments)
+	}
+
+	r2uo := hashmap.NewComparable[sharing.ID, *Round2P2P]()
+	for sharingId := range p.quorum.Iter() {
+		if sharingId == p.mySharingId {
+			continue
+		}
+
+		seedContribution, ok := p.state.seedContributions.Get(sharingId)
+		if !ok {
+			return nil, errs.NewValidation("missing seed contribution to %d", sharingId)
+		}
+
+		witness, ok := p.state.witnesses.Get(sharingId)
+		if !ok {
+			return nil, errs.NewValidation("missing seed witness to %d", sharingId)
+		}
+		
+		r2uo.Put(sharingId, &Round2P2P{
+			seedContribution: seedContribution,
+			witness:          witness,
+		})
+	}
+
+	return r2uo.Freeze(), nil
+}
+
+func (p *Participant) Round3(r2uo network.RoundMessages[*Round2P2P]) (przs.Seeds, error) {
+	commonSeeds := hashmap.NewComparable[sharing.ID, [32]byte]()
+	for sharingId := range p.quorum.Iter() {
+		if sharingId == p.mySharingId {
+			continue
+		}
+
+		msg, ok := r2uo.Get(sharingId)
+		if !ok {
+			return nil, errs.NewValidation("missing message from %d", sharingId)
+		}
+		theirSeedContribution := msg.seedContribution
+		theirWitness := msg.witness
+		theirCommitments, ok := p.state.commitments.Get(sharingId)
+		if !ok {
+			return nil, errs.NewValidation("missing commitments from %d", sharingId)
+		}
+		theirCommitment, ok := theirCommitments.Get(p.mySharingId)
+		if !ok {
+			return nil, errs.NewValidation("missing commitment for %d", sharingId)
+		}
+		if p.state.commitmentScheme.Verifier().Verify(theirCommitment, theirSeedContribution[:], theirWitness) != nil {
+			return nil, errs.NewIdentifiableAbort(sharingId, "invalid seed contribution")
+		}
+		mySeedContribution, ok := p.state.seedContributions.Get(sharingId)
+		var seed [32]byte
+		subtle.XORBytes(seed[:], theirSeedContribution[:], mySeedContribution[:])
+		commonSeeds.Put(sharingId, seed)
+	}
+
+	return commonSeeds.Freeze(), nil
+}
