aeb349fe04ea004af291f0539e942d35ffd13c46
Mateusz Kramarczyk <mateusz.kramarczyk@bron.xyz>
2025-08-29 11:55:57 +0200

    Fix randomization.

--- pkg/threshold/tsig/tecdsa/dkls23/signing/interactive/sign_softspoken/participant.go
+++ pkg/threshold/tsig/tecdsa/dkls23/signing/interactive/sign_softspoken/participant.go
@@ -176,7 +176,7 @@ func randomizeZeroSeeds(seeds przs.Seeds, tape transcripts.Transcript) (przs.See
 }
 
 func randomizeOTSeeds(senderSeeds ds.Map[sharing.ID, *vsot.SenderOutput], receiverSeeds ds.Map[sharing.ID, *vsot.ReceiverOutput], tape transcripts.Transcript) (ds.Map[sharing.ID, *vsot.SenderOutput], ds.Map[sharing.ID, *vsot.ReceiverOutput], error) {
-	randomizerKey, err := tape.ExtractBytes(przsRandomizerLabel, (2*base.ComputationalSecurity+7)/8)
+	randomizerKey, err := tape.ExtractBytes(otRandomizerLabel, (2*base.ComputationalSecurity+7)/8)
 	if err != nil {
 		return nil, nil, errs.WrapFailed(err, "cannot extract randomizer")
 	}
