76f5115057dff5cdb03fcafe2927735848b3cec4
Alireza Rafiei <alireza.rafiei@bron.xyz>
2025-11-18 01:54:31 +0000

    rfc8937

--- pkg/base/prng/csprng/rfc8937/rfc8937.go
+++ pkg/base/prng/csprng/rfc8937/rfc8937.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/bronlabs/bron-crypto/pkg/base/algebra"
 	"github.com/bronlabs/bron-crypto/pkg/base/curves"
-	"github.com/bronlabs/bron-crypto/pkg/base/errs"
+	"github.com/bronlabs/bron-crypto/pkg/base/errs2"
 	"github.com/bronlabs/bron-crypto/pkg/hashing"
 	"github.com/bronlabs/bron-crypto/pkg/signatures/ecdsa"
 )
@@ -26,16 +26,16 @@ type WrappedReader struct {
 
 func Wrap[P curves.Point[P, B, S], B algebra.PrimeFieldElement[B], S algebra.PrimeFieldElement[S]](prng io.Reader, signer *ecdsa.Signer[P, B, S], uniqueDeviceId []byte) (*WrappedReader, error) {
 	if !signer.IsDeterministic() {
-		return nil, errs.NewFailed("signer must be deterministic")
+		return nil, errs2.Wrap(ErrSignerDeterminism)
 	}
 
 	sig, err := signer.Sign(uniqueDeviceId)
 	if err != nil {
-		return nil, errs.WrapFailed(err, "could not sign unique device id")
+		return nil, errs2.Wrap(ErrUniqueDeviceIDSignature)
 	}
 	salt, err := hashing.Hash(hashFunc, slices.Concat(sig.R().Bytes(), sig.S().Bytes()))
 	if err != nil {
-		return nil, errs.WrapHashing(err, "could not hash signed unique device id")
+		return nil, errs2.Wrap(ErrHashingUniqueDeviceID)
 	}
 
 	return &WrappedReader{
@@ -49,18 +49,27 @@ func (r *WrappedReader) Read(p []byte) (n int, err error) {
 	g := make([]byte, l)
 	_, err = io.ReadFull(r.wrapee, g)
 	if err != nil {
-		return n, errs.WrapRandomSample(err, "could not read wrapped reader")
+		return n, errs2.Wrap(ErrRandomSample)
 	}
 	key, err := hkdf.Extract(hashFunc, g, r.salt)
 	if err != nil {
-		return 0, errs.WrapFailed(err, "could not extract key")
+		return 0, errs2.Wrap(ErrExtractKey)
 	}
 
 	tag2 := r.counter.Add(1)
 	gPrime, err := hkdf.Expand(hashFunc, key, fmt.Sprintf("%d", tag2), len(p))
 	if err != nil {
-		return 0, errs.WrapFailed(err, "could not expand key")
+		return 0, errs2.Wrap(ErrExpandKey)
 	}
 	copy(p, gPrime)
 	return len(gPrime), nil
 }
+
+var (
+	ErrSignerDeterminism       = errs2.New("signer must be deterministic")
+	ErrUniqueDeviceIDSignature = errs2.New("could not sign unique device id")
+	ErrHashingUniqueDeviceID   = errs2.New("could not hash signed unique device id")
+	ErrRandomSample            = errs2.New("could not read from wrapped reader")
+	ErrExtractKey              = errs2.New("could not extract key")
+	ErrExpandKey               = errs2.New("could not expand key")
+)
