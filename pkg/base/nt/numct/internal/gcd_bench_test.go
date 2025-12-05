9b4536c9c7c3fa7eae6c5bffab3a96593fef7842
Mateusz Kramarczyk <106983829+mkramarczyk@users.noreply.github.com>
2025-12-05 15:19:39 +0100

    Add constant-time GCD. (#84)

---
+++ pkg/base/nt/numct/internal/gcd_bench_test.go
@@ -0,0 +1,30 @@
+package internal_test
+
+import (
+	crand "crypto/rand"
+	"math/big"
+	"testing"
+
+	"github.com/bronlabs/bron-crypto/pkg/base/nt/numct/internal"
+	"github.com/cronokirby/saferith"
+	"github.com/stretchr/testify/require"
+)
+
+func Benchmark_GCD(b *testing.B) {
+	const bits = 4096
+	prng := crand.Reader
+
+	boundBig := new(big.Int)
+	boundBig.SetBit(boundBig, bits, 1)
+	xBig, err := crand.Int(prng, boundBig)
+	require.NoError(b, err)
+	yBig, err := crand.Int(prng, boundBig)
+
+	x := new(saferith.Nat).SetBig(xBig, bits)
+	y := new(saferith.Nat).SetBig(yBig, bits)
+
+	b.ResetTimer()
+	for range b.N {
+		_ = internal.GCD(x, y)
+	}
+}
