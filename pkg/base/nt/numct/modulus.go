921eb1460bafbe3fdb120e2bd922854e8d65157b
Mateusz Kramarczyk <mateusz.kramarczyk@bron.xyz>
2025-12-10 14:07:45 +0100

    fix comment

--- pkg/base/nt/numct/modulus.go
+++ pkg/base/nt/numct/modulus.go
@@ -304,7 +304,7 @@ func (m *ModulusBasic) IsInRange(x *Nat) ct.Bool {
 	return ct.Bool(lt)
 }
 
-// IsInRangeSymmetric returns true if -m/2 <= x <= m/2.
+// IsInRangeSymmetric returns true if -m/2 <= x < m/2.
 func (m *ModulusBasic) IsInRangeSymmetric(x *Int) ct.Bool {
 	mod := (*Nat)(((*saferith.Modulus)(m)).Nat()).Lift()
 	var x2, modNeg Int
