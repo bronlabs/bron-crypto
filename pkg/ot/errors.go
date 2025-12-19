1b93fb573dd8c19436b6dc10203db2590102ece3
Mateusz Kramarczyk <mateusz.kramarczyk@bron.xyz>
2025-12-19 13:06:19 +0100

    refactor aborts

--- pkg/ot/errors.go
+++ pkg/ot/errors.go
@@ -5,7 +5,5 @@ import "github.com/bronlabs/bron-crypto/pkg/base/errs2"
 var (
 	ErrInvalidArgument = errs2.New("invalid argument")
 	ErrFailed          = errs2.New("failed")
-	ErrSerialisation   = errs2.New("serialisation failed")
 	ErrRound           = errs2.New("invalid round")
-	ErrVerification    = errs2.New("verification failed")
 )
