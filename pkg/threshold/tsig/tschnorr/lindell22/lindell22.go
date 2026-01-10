50dccffa3f91a6f3bfac96266a204e8f0d3bdd05
Alireza Rafiei <alireza.rafiei@bron.xyz>
2026-01-10 10:47:47 +0000

    lindell22

--- pkg/threshold/tsig/tschnorr/lindell22/lindell22.go
+++ pkg/threshold/tsig/tschnorr/lindell22/lindell22.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/bronlabs/bron-crypto/pkg/base/algebra"
 	ds "github.com/bronlabs/bron-crypto/pkg/base/datastructures"
-	"github.com/bronlabs/bron-crypto/pkg/base/errs"
+	"github.com/bronlabs/bron-crypto/pkg/base/errs2"
 	hash_comm "github.com/bronlabs/bron-crypto/pkg/commitments/hash"
 	"github.com/bronlabs/bron-crypto/pkg/network"
 	"github.com/bronlabs/bron-crypto/pkg/threshold/sharing"
@@ -16,33 +16,42 @@ import (
 const commitmentDomainRLabel = "Lindell2022SignR-"
 
 type (
-	Shard[GE algebra.PrimeGroupElement[GE, S], S algebra.PrimeFieldElement[S]]            = tschnorr.Shard[GE, S]
-	PublicMaterial[GE algebra.PrimeGroupElement[GE, S], S algebra.PrimeFieldElement[S]]   = tschnorr.PublicMaterial[GE, S]
+	// Shard represents a party's secret key share for threshold Schnorr signing.
+	Shard[GE algebra.PrimeGroupElement[GE, S], S algebra.PrimeFieldElement[S]] = tschnorr.Shard[GE, S]
+	// PublicMaterial contains public information shared among signing parties.
+	PublicMaterial[GE algebra.PrimeGroupElement[GE, S], S algebra.PrimeFieldElement[S]] = tschnorr.PublicMaterial[GE, S]
+	// PartialSignature is a party's contribution to the threshold signature.
 	PartialSignature[GE algebra.PrimeGroupElement[GE, S], S algebra.PrimeFieldElement[S]] = tschnorr.PartialSignature[GE, S]
 )
 
 // Lindell22 is proven to be secure in standard model only if a UC-secure commitment scheme is used.
-// due to CF01, no such scheme can exist without a trusted setup/interaction. So we use a hash-based commitment scheme
-// to save some rounds. Implication is having to rely on RO.
+// Due to CF01, no such scheme can exist without a trusted setup/interaction. So we use a hash-based
+// commitment scheme to save some rounds. Implication is having to rely on RO.
 type (
+	// CommitmentScheme is the hash-based commitment scheme used in the protocol.
 	CommitmentScheme = hash_comm.Scheme
-	Commitment       = hash_comm.Commitment
-	Opening          = hash_comm.Witness
-	CommitmentKey    = hash_comm.Key
+	// Commitment is a hiding commitment to a value.
+	Commitment = hash_comm.Commitment
+	// Opening contains the witness needed to open a commitment.
+	Opening = hash_comm.Witness
+	// CommitmentKey is the public key for the commitment scheme.
+	CommitmentKey = hash_comm.Key
 )
 
+// NewCommitmentScheme creates a new hash-based commitment scheme.
 var NewCommitmentScheme = hash_comm.NewScheme
 
+// NewCommitmentKey creates a commitment key from session ID, party ID, and quorum information.
 func NewCommitmentKey(sid network.SID, pid sharing.ID, quorumBytes [][]byte) (CommitmentKey, error) {
 	pidBytes := binary.BigEndian.AppendUint64(nil, uint64(pid))
 	key, err := hash_comm.NewKeyFromCRSBytes(sid, commitmentDomainRLabel, append([][]byte{pidBytes}, quorumBytes...)...)
 	if err != nil {
-		return *new(CommitmentKey), errs.WrapFailed(err, "cannot create key for commitment scheme")
+		return *new(CommitmentKey), errs2.Wrap(err).WithMessage("cannot create key for commitment scheme")
 	}
 	return key, nil
 }
 
-// sorted quorum IDs in big-endian format, used for commitment scheme CRS and dlog proofs.
+// QuorumBytes returns sorted quorum IDs in big-endian format, used for commitment scheme CRS and dlog proofs.
 func QuorumBytes(quorum ds.Set[sharing.ID]) [][]byte {
 	if quorum == nil || quorum.Size() == 0 {
 		return nil
