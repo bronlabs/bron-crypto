4cefacd6bebac13bdd01602a972351505368046c
Alireza Rafiei <aalireza@users.noreply.github.com>
2026-01-12 14:12:27 +0000

    Lindell22+tsig (#126)
    * lindell22
    
    * lindell22 and tsig

--- pkg/threshold/tsig/tschnorr/lindell22/signing/rounds.go
+++ pkg/threshold/tsig/tschnorr/lindell22/signing/rounds.go
@@ -4,7 +4,7 @@ import (
 	"encoding/binary"
 
 	"github.com/bronlabs/bron-crypto/pkg/base/algebra"
-	"github.com/bronlabs/bron-crypto/pkg/base/errs"
+	"github.com/bronlabs/bron-crypto/pkg/base/errs2"
 	"github.com/bronlabs/bron-crypto/pkg/base/utils/algebrautils"
 	"github.com/bronlabs/bron-crypto/pkg/network"
 	schnorrpok "github.com/bronlabs/bron-crypto/pkg/proofs/dlog/schnorr"
@@ -18,9 +18,10 @@ import (
 
 const transcriptDLogSLabel = "Lindell2022SignDLogS-"
 
+// Round1 samples a random nonce, commits to it, and broadcasts the commitment.
 func (c *Cosigner[E, S, M]) Round1() (*Round1Broadcast, error) {
 	if c.round != 1 {
-		return nil, errs.NewRound("Running round %d but participant expected round %d", 1, c.round)
+		return nil, ErrInvalidRound.WithMessage("Running round %d but participant expected round %d", 1, c.round)
 	}
 
 	// step 1.1: Sample k_i <-$- ℤ_q  &  compute R_i = k_i * G
@@ -28,14 +29,14 @@ func (c *Cosigner[E, S, M]) Round1() (*Round1Broadcast, error) {
 	// Any usable single party schnorr variant will have extra methods to allows us to correct parity and alike later.
 	k, err := algebrautils.RandomNonIdentity(c.sf, c.prng)
 	if err != nil {
-		return nil, errs.NewFailed("cannot create randomised nonce commitment")
+		return nil, errs2.Wrap(err).WithMessage("cannot create randomised nonce commitment")
 	}
 	bigR := c.group.ScalarBaseOp(k)
 
 	// step 1.2: Run c_i <= commit(sid || R_i || i || S)
 	commitment, opening, err := commitBigR(c, bigR)
 	if err != nil {
-		return nil, errs.NewFailed("cannot commit to R")
+		return nil, errs2.Wrap(err).WithMessage("cannot commit to R")
 	}
 
 	// step 1.4: Broadcast(c_i)
@@ -51,9 +52,10 @@ func (c *Cosigner[E, S, M]) Round1() (*Round1Broadcast, error) {
 	return broadcast, nil
 }
 
+// Round2 receives commitments from other parties and broadcasts the nonce with a discrete log proof.
 func (c *Cosigner[E, S, M]) Round2(inb network.RoundMessages[*Round1Broadcast]) (*Round2Broadcast[E, S], error) {
 	if c.round != 2 {
-		return nil, errs.NewRound("Running round %d but participant expected round %d", 2, c.round)
+		return nil, ErrInvalidRound.WithMessage("Running round %d but participant expected round %d", 2, c.round)
 	}
 	for pid := range c.quorum.Iter() {
 		if c.SharingID() == pid {
@@ -66,7 +68,7 @@ func (c *Cosigner[E, S, M]) Round2(inb network.RoundMessages[*Round1Broadcast]) 
 	c.state.tapeFrozenBeforeDlogProof = c.tape.Clone()
 	bigRProof, statement, err := dlogProve(c, c.state.k, c.state.bigR, c.state.quorumBytes)
 	if err != nil {
-		return nil, errs.WrapFailed(err, "cannot prove dlog")
+		return nil, errs2.Wrap(err).WithMessage("cannot prove dlog")
 	}
 
 	// step 2.3: Broadcast(π^dl_i, R_i, c_i)
@@ -80,9 +82,10 @@ func (c *Cosigner[E, S, M]) Round2(inb network.RoundMessages[*Round1Broadcast]) 
 	return broadcast, nil
 }
 
+// Round3 verifies other parties' commitments and proofs, then computes the partial signature.
 func (c *Cosigner[E, S, M]) Round3(inb network.RoundMessages[*Round2Broadcast[E, S]], message M) (*tschnorr.PartialSignature[E, S], error) {
 	if c.round != 3 {
-		return nil, errs.NewRound("Running round %d but participant expected round %d", 3, c.round)
+		return nil, ErrInvalidRound.WithMessage("Running round %d but participant expected round %d", 3, c.round)
 	}
 	summedR := c.state.bigR
 	for pid := range c.quorum.Iter() {
@@ -91,18 +94,17 @@ func (c *Cosigner[E, S, M]) Round3(inb network.RoundMessages[*Round2Broadcast[E,
 		}
 		received, _ := inb.Get(pid)
 		theirBigR := received.BigR
-		// theirBigR.Phi = c.state.phi // ensure the phi is set in the statement
 		theirOpening := received.BigROpening
 		theirCommitment := c.state.theirBigRCommitments[pid]
 		// step 3.2: Open(sid || R_j || j || S)
 		if err := verifyBigRCommitment(c, pid, theirBigR.X, theirOpening, theirCommitment); err != nil {
-			return nil, errs.WrapIdentifiableAbort(err, pid, "cannot verify commitment for participant")
+			return nil, errs2.Wrap(err).WithTag(errs2.IdentifiableAbortPartyId, pid).WithMessage("cannot verify commitment for participant")
 		}
 		// step 3.3: Run NIPoKDL.Verify(R_j, π^dl_j)
 		if err := dlogVerify(
 			c.state.tapeFrozenBeforeDlogProof.Clone(), c.niDlogScheme, pid, c.sid, received.BigRProof, theirBigR, c.state.quorumBytes,
 		); err != nil {
-			return nil, errs.WrapIdentifiableAbort(err, pid, "cannot verify dlog proof for participant")
+			return nil, errs2.Wrap(err).WithTag(errs2.IdentifiableAbortPartyId, pid).WithMessage("cannot verify dlog proof for participant")
 		}
 		// step 3.4: R <- Σ R_j
 		summedR = summedR.Op(theirBigR.X)
@@ -110,12 +112,12 @@ func (c *Cosigner[E, S, M]) Round3(inb network.RoundMessages[*Round2Broadcast[E,
 	// step 3.7.2: compute e
 	e, err := c.variant.ComputeChallenge(summedR, c.shard.PublicKey().Value(), message)
 	if err != nil {
-		return nil, errs.NewFailed("cannot create digest scalar")
+		return nil, errs2.Wrap(err).WithMessage("cannot create digest scalar")
 	}
 
 	psig, err := c.ComputePartialSignature(summedR, e)
 	if err != nil {
-		return nil, errs.WrapFailed(err, "cannot compute partial signature")
+		return nil, errs2.Wrap(err).WithMessage("cannot compute partial signature")
 	}
 	c.round++
 	return psig, nil
@@ -126,16 +128,16 @@ func commitBigR[
 ](c *Cosigner[E, S, M], bigR E) (lindell22.Commitment, lindell22.Opening, error) {
 	key, err := lindell22.NewCommitmentKey(c.sid, c.SharingID(), c.state.quorumBytes)
 	if err != nil {
-		return lindell22.Commitment{}, lindell22.Opening{}, errs.WrapFailed(err, "cannot create commitment key")
+		return lindell22.Commitment{}, lindell22.Opening{}, errs2.Wrap(err).WithMessage("cannot create commitment key")
 	}
 	// step 1.2: Run c_i <= commit(sid || R_i || i || S)
 	commitmentScheme, err := lindell22.NewCommitmentScheme(key)
 	if err != nil {
-		return lindell22.Commitment{}, lindell22.Opening{}, errs.WrapFailed(err, "cannot create commitment scheme")
+		return lindell22.Commitment{}, lindell22.Opening{}, errs2.Wrap(err).WithMessage("cannot create commitment scheme")
 	}
 	commitment, opening, err := commitmentScheme.Committer().Commit(bigR.Bytes(), c.prng)
 	if err != nil {
-		return lindell22.Commitment{}, lindell22.Opening{}, errs.WrapFailed(err, "cannot commit to R")
+		return lindell22.Commitment{}, lindell22.Opening{}, errs2.Wrap(err).WithMessage("cannot commit to R")
 	}
 	return commitment, opening, nil
 }
@@ -145,14 +147,14 @@ func verifyBigRCommitment[
 ](c *Cosigner[E, S, M], theirID sharing.ID, theirBigR E, theirOpening lindell22.Opening, theirCommitment lindell22.Commitment) error {
 	key, err := lindell22.NewCommitmentKey(c.sid, theirID, c.state.quorumBytes)
 	if err != nil {
-		return errs.WrapFailed(err, "cannot create commitment key for participant %d", theirID)
+		return errs2.Wrap(err).WithMessage("cannot create commitment key for participant %d", theirID)
 	}
 	commitmentScheme, err := lindell22.NewCommitmentScheme(key)
 	if err != nil {
-		return errs.WrapFailed(err, "cannot create commitment scheme for participant %d", theirID)
+		return errs2.Wrap(err).WithMessage("cannot create commitment scheme for participant %d", theirID)
 	}
 	if err := commitmentScheme.Verifier().Verify(theirCommitment, theirBigR.Bytes(), theirOpening); err != nil {
-		return errs.WrapVerification(err, "cannot verify commitment for participant %d", theirID)
+		return errs2.Wrap(err).WithMessage("cannot verify commitment for participant %d", theirID)
 	}
 	return nil
 }
@@ -165,7 +167,7 @@ func dlogProve[
 	c.tape.AppendBytes("prover", proverIDBytes)
 	prover, err := c.niDlogScheme.NewProver(c.sid, c.tape)
 	if err != nil {
-		return nil, nil, errs.WrapFailed(err, "cannot create dlog prover")
+		return nil, nil, errs2.Wrap(err).WithMessage("cannot create dlog prover")
 	}
 	statement := &schnorrpok.Statement[E, S]{
 		X: bigR,
@@ -175,7 +177,7 @@ func dlogProve[
 	}
 	proof, err := prover.Prove(statement, witness)
 	if err != nil {
-		return nil, nil, errs.WrapFailed(err, "cannot create dlog proof")
+		return nil, nil, errs2.Wrap(err).WithMessage("cannot create dlog proof")
 	}
 	return proof, statement, nil
 }
@@ -188,10 +190,10 @@ func dlogVerify[
 	tape.AppendBytes("prover", proverIDBytes)
 	verifier, err := niDlogScheme.NewVerifier(sid, tape)
 	if err != nil {
-		return errs.WrapFailed(err, "cannot create dlog verifier")
+		return errs2.Wrap(err).WithMessage("cannot create dlog verifier")
 	}
 	if err := verifier.Verify(theirBigR, proof); err != nil {
-		return errs.WrapVerification(err, "cannot verify dlog proof for participant %d", proverID)
+		return errs2.Wrap(err).WithMessage("cannot verify dlog proof for participant %d", proverID)
 	}
 	return nil
 }
