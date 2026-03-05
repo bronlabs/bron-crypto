70407999eb8c61abd4c629233c39a283fd77bb8d
Mateusz Kramarczyk <106983829+mkramarczyk@users.noreply.github.com>
2026-03-05 19:10:58 +0100

    Add Birkhoff interpolation in exponent. (#193)
    * Add Birkhoff interpolation in exponent.
    
    * rename

--- pkg/base/polynomials/interpolation/birkhoff/birkhoff_test.go
+++ pkg/base/polynomials/interpolation/birkhoff/birkhoff_test.go
@@ -18,6 +18,12 @@ func Test_HappyPath(t *testing.T) {
 	testHappyPath(t, k256.NewScalarField())
 }
 
+func Test_HappyPathInExponent(t *testing.T) {
+	t.Parallel()
+
+	testHappyPathInExp(t, k256.NewCurve())
+}
+
 // Test_UnmetCriteria fails to uniquely determine a degree-3 polynomial because matrix A(E,X,\phi) is singular.
 func Test_UnmetCriteria(t *testing.T) {
 	t.Parallel()
@@ -118,4 +124,55 @@ func testUnmetCriteria[F algebra.PrimeFieldElement[F]](tb testing.TB, field alge
 
 	_, err = birkhoff.Interpolate(xs, js, ys)
 	require.Error(tb, err)
+}
+
+func testHappyPathInExp[G algebra.PrimeGroupElement[G, F], F algebra.PrimeFieldElement[F]](tb testing.TB, group algebra.PrimeGroup[G, F]) {
+	tb.Helper()
+
+	prng := pcg.NewRandomised()
+	field := algebra.StructureMustBeAs[algebra.PrimeField[F]](group.ScalarStructure())
+	polyModule, err := polynomials.NewPolynomialModule(group)
+	require.NoError(tb, err)
+
+	c0, err := field.Random(prng)
+	require.NoError(tb, err)
+	c1, err := field.Random(prng)
+	require.NoError(tb, err)
+	c2, err := field.Random(prng)
+	require.NoError(tb, err)
+	c3, err := field.Random(prng)
+	require.NoError(tb, err)
+
+	g := group.Generator()
+	expPoly, err := polyModule.New(
+		g.ScalarOp(c0),
+		g.ScalarOp(c1),
+		g.ScalarOp(c2),
+		g.ScalarOp(c3),
+	)
+	require.NoError(tb, err)
+
+	xs := []F{
+		field.FromUint64(1),
+		field.FromUint64(2),
+		field.FromUint64(2),
+		field.FromUint64(3),
+		field.FromUint64(3),
+		field.FromUint64(4),
+		field.FromUint64(4),
+	}
+	js := []uint64{0, 0, 1, 1, 2, 2, 3}
+	ys := []G{
+		expPoly.Eval(xs[0]),
+		expPoly.Eval(xs[1]),
+		expPoly.Derivative().Eval(xs[2]),
+		expPoly.Derivative().Eval(xs[3]),
+		expPoly.Derivative().Derivative().Eval(xs[4]),
+		expPoly.Derivative().Derivative().Eval(xs[5]),
+		expPoly.Derivative().Derivative().Derivative().Eval(xs[6]),
+	}
+
+	interpolated, err := birkhoff.InterpolateInExponent(xs, js, ys)
+	require.NoError(tb, err)
+	require.True(tb, expPoly.Equal(interpolated))
 }
