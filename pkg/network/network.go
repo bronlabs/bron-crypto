f65b2e9c9f592b4ef6b10ce82f3bc58396af2ccb
Alireza Rafiei <alireza.rafiei@bron.xyz>
2026-03-17 21:26:32 +0000

    network

--- pkg/network/network.go
+++ pkg/network/network.go
@@ -17,11 +17,11 @@ type SID [32]byte
 // NewSID hashes the provided byte slices to produce a 32-byte session identifier.
 func NewSID(xs ...[]byte) (SID, error) {
 	digest, err := hashing.Hash(sidHasher, xs...)
-	if len(digest) != 32 {
-		return SID{}, ErrInvalidArgument.WithMessage("digest length is not 32 bytes")
-	}
 	if err != nil {
 		return SID{}, errs.Wrap(err).WithMessage("failed to create session ID")
+	}
+	if len(digest) != 32 {
+		return SID{}, ErrInvalidArgument.WithMessage("digest length is not 32 bytes")
 	}
 	var sid SID
 	copy(sid[:], digest)
